4753dbcba06f60b7bd4db1c2376e10e648e6974d
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-03-24 21:12:42 +0100

    refactored day 18 part 1 to use tokenizer

--- 18/main.c
+++ 18/main.c
@@ -4,97 +4,170 @@
 #include <assert.h>
 #include <ctype.h>
 
-
 int tokens_read;
 
-unsigned long long eval(char *input) {
-    char *s = input;
-    char buf[256];
-    buf[0] = '\0';
-    char *tmp = buf;
-    unsigned long long left, right;
-    char op;
+typedef struct token {
+    enum token_type {
+        TOK_NUMBER,
+        TOK_PLUS,
+        TOK_ASTERISK,
+        TOK_LPAREN,
+        TOK_RPAREN,
+        TOK_EOF
+    } type;
+    int value;
+} token_t;
 
+char *token_names[] = {
+    "NUMBER",
+    "PLUS",
+    "ASTERISK",
+    "LPAREN",
+    "RPAREN",
+    "EOF",
+};
+
+char *s;
+
+token_t
+gettoken() {
     while (isspace(*s)) s++;
 
-    if (*s == '(') {
-        s++; // (
-        left = eval(s);
-        s += tokens_read;
-        s++; // )
-    } else {
-        // parse digit
-        while (*s >= '0' && *s <= '9') {
-            *tmp++ = *s++;
-        }
-        *tmp = '\0';
-        left = strtol(buf, 0, 10);
-        tmp = buf;
+    switch (*s) {
+        case '(':
+            s++;
+            return (token_t) {
+                .type = TOK_LPAREN,
+            };
+            break;
+        case ')':
+            s++;
+            return (token_t) {
+                .type = TOK_RPAREN,
+            };
+            break;
+        case '*':
+            s++;
+            return (token_t) {
+                .type = TOK_ASTERISK,
+            };
+            break;
+        case '+':
+            s++;
+            return (token_t) {
+                .type = TOK_PLUS,
+            };
+            break;
+        case '\0':
+            return (token_t ) {
+                .type = TOK_EOF,
+            };
+            break;
+        default:
+            if (!isdigit(*s)) {
+                break;
+            }
+
+            char buf[64];
+            char *n = buf;
+            while (isdigit(*s)) {
+                *n++ = *s++;
+            }
+            *n++ = '\0';
+            return (token_t) {
+                .type = TOK_NUMBER,
+                .value = strtol(buf, NULL, 10),
+            };
+        break;
     }
 
-    while (*s != '\0') {
-        // parse operator
-        while (isspace(*s)) s++;
-        op = *s++;
-        if (op != '+' && op != '*') {
-            err(EXIT_FAILURE, "invalid operator: %c", op);
-        }
-        while (isspace(*s)) s++;
+    err(EXIT_FAILURE, "Unknown token at '%s'", s);
+}
 
-        // parse right
-        if (*s == '(') {
-            s++; // (
-            right = eval(s);
-            s += tokens_read;
-            s++; // )
-        } else {
-            // parse digit
-            while (*s >= '0' && *s <= '9') {
-                *tmp++ = *s++;
-            }
-            *tmp = '\0';
-            right = strtol(buf, 0, 10);
-            tmp = buf;
+token_t
+nexttoken() {
+    char *tmp = s;
+    token_t tok = gettoken();
+    s = tmp;
+    return tok;
+}
+
+void
+expect(enum token_type type) {
+    token_t tok = gettoken();
+    if (tok.type != type) {
+        err(EXIT_FAILURE, "expected token of type %s, got %s", token_names[type], token_names[tok.type]);
+    }
+}
+
+unsigned long long _eval() {
+    unsigned long long left = 0;
+    unsigned long long right = 0;
+
+    token_t tok = gettoken();
+    switch (tok.type) {
+        case TOK_NUMBER:
+            left = tok.value;
+            break;
+        case TOK_LPAREN:
+            left = _eval();
+            break;
+        default:
+            err(EXIT_FAILURE, "Invalid token: %s\n", token_names[tok.type]);
+            break;
+    }
+
+
+    while (tok.type != TOK_EOF && nexttoken().type != TOK_EOF) {
+        token_t op = gettoken();
+        if (op.type != TOK_ASTERISK && op.type != TOK_PLUS) {
+            err(EXIT_FAILURE, "expected operator, got %s\n", token_names[op.type]);
         }
-        while (isspace(*s)) s++;
         
-        // do operation
-        switch (op) {
-            case '+':
-                left += right;
+        tok = gettoken();
+        switch (tok.type) {
+            case TOK_NUMBER:
+                right = tok.value;
                 break;
-            case '*':
+
+            case TOK_LPAREN:
+                right = _eval();
+                break;
+
+            default: break;
+        }
+
+        switch (op.type) {
+            case TOK_ASTERISK:
                 left *= right;
                 break;
-            // case '-':
-            //     left -= right;
-            //     break;
-            // case '/':
-            //     left /= right;
-            //     break;
-            default:
-                err(EXIT_FAILURE, "invalid operator: %c\n", op);
-                break;
-        }
 
-        if (*s == ')') {
-            break;
+            case TOK_PLUS:
+                left += right;
+                break;
+
+            default: break;
+        }   
+
+        if (nexttoken().type == TOK_RPAREN) {
+            gettoken();
+            return left;
         }
     }
 
-    tokens_read = s - input;
+
     return left;
 }
 
+unsigned long long eval(char *input) {
+    s = input;
+   return _eval();
+}
+
+
 int main() {
     assert(eval("1 + 2 * 3 + 4 * 5 + 6") == 71);
-    assert(eval("(1 + 2)") == 3);
-    assert(eval("((1 + 2) * 3)") == 9);
-    assert(eval("1 + 2 * 3") == 9);
-    assert(eval("1 + (2 * 3)") == 7);
-    assert(eval("1 + (1*(1*(2 * 3)))") == 7);
     assert(eval("2 * 3 + (4 * 5)") == 26);
-    assert(eval("1 + (2 * 3) + (4 * (5 + 6))") == 51);
     assert(eval("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437);
     assert(eval("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240);
     assert(eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632);

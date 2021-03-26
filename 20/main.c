e82cd8b4d9cd5a1426d5b02c407cd3734c5ba637
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-03-26 12:04:24 +0100

    day 20 / part 1. 'cheated' by not assembling the image, but just finding the corners.

--- 20/main.c
+++ 20/main.c
@@ -3,12 +3,19 @@
 #include <err.h>
 #include <assert.h>
 #include <string.h>
+#include <math.h>
+
+typedef struct tile tile_t;
 
 struct tile {
     int id;
     char grid[100];
+
+    tile_t *n;
+    tile_t *e;
+    tile_t *s;
+    tile_t *w;
 };
-typedef struct tile tile_t;
 
 #define W 10
 #define H 10
@@ -16,7 +23,7 @@ typedef struct tile tile_t;
 int 
 parse_tiles_from_input(tile_t *tiles) {
     int ntiles = 0;
-    FILE *f = fopen("test_input.txt", "r");
+    FILE *f = fopen("input.txt", "r");
     if (!f) err(EXIT_FAILURE, "error reading input file");
     char linebuf[BUFSIZ] = {0};
     while (fgets(linebuf, BUFSIZ, f) != NULL) {
@@ -106,10 +113,43 @@ cmp_tiles(tile_t *t1, tile_t *t2) {
     return 0; // grids match
 }
 
+int
+cmp_tile_edges(tile_t *t1, tile_t *t2, char edge) {
+    int x, y;
+
+    // edge can be one of 'N', 'E', 'S', 'W'
+    switch (edge) {
+        case 'N':
+        case 'S':
+            y = edge == 'N' ? 0 : H-1;
+            for (x=0; x < W; x++) {
+                if (t1->grid[y * W + x] != t2->grid[y * W + x]) {
+                    return 1;
+                }
+            }
+            return 0;
+        break;
+
+        case 'E':
+        case 'W':
+            x = edge == 'E' ? W-1 : 0;
+            for (y=0; y < H; y++) {
+                if (t1->grid[y * W + x] != t2->grid[y * W + x]) {
+                    return 1;
+                }
+            }
+            return 0;
+        break;  
+
+        default:
+            err(EXIT_FAILURE, "invalid edge argument: %c", edge);
+        break;  
+    }
+}
 
 
 int main() {
-    tile_t *tiles = malloc(9 * sizeof(tile_t));
+    tile_t *tiles = malloc(200 * sizeof(tile_t));
     if (!tiles) {
         err(EXIT_FAILURE, "could not allocate memory for tiles");
     }
@@ -133,5 +173,70 @@ int main() {
     #endif
 
 
+    // init empty image
+    int img_size = (int) sqrt((double) ntiles);
+    printf("Image size is %d x %d\n", img_size, img_size);
+    int *image = calloc(img_size * img_size, sizeof(int));
+    for (int y = 0; y < img_size; y++) {
+        for (int x = 0; x < img_size; x++) {
+            printf("%d\t", image[y * img_size + x]);
+        }
+        printf("\n");
+    }
+
+    // start by finding the middle tile
+    // which is a tile that should have 4 possible neighbors
+    printf("%d\n", 3/2);
+    long long product = 1;
+    for (int i=0; i < ntiles; i++) {
+        tile_t *t1 = &tiles[i];
+
+        printf("Finding neighbors for %d: \n", t1->id);
+        int neighbors = 0;
+
+        for (int j=0; j < ntiles; j++) {
+            // skip self
+            if (i == j) { continue; }
+            tile_t *t2 = &tiles[j];
+
+            for (int r=0; r < 4; r++) {
+                for (int f=0; f < 2; f++) {
+                    if (cmp_tile_edges(t1, t2, 'N') == 0) {
+                        printf("Found N: %d\n", t2->id);
+                        t1->n = t2;
+                        neighbors++;
+                    } else if (cmp_tile_edges(t1, t2, 'E') == 0) {
+                        printf("Found E: %d\n", t2->id);
+                        t1->e = t2;
+                        neighbors++;
+                    } else if (cmp_tile_edges(t1, t2, 'S') == 0) {
+                        printf("Found S: %d\n", t2->id);
+                        t1->s = t2;
+                        neighbors++;
+                    } else if (cmp_tile_edges(t1, t2, 'W') == 0) {
+                        printf("Found W: %d\n", t2->id);
+                        t1->w = t2;
+                        neighbors++;
+                    }     
+
+                    flip_tile(t2, f > 0 ? 'x' : 'y');
+                }
+
+                rotate_tile(t2);   
+            }    
+        }
+
+        if (neighbors == 2) {
+            product *= t1->id;
+        }
+
+        
+    }
+
+    printf("Product: %lld\n", product);
+
+
+
+    free(image);
     free(tiles);
 }

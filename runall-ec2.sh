df4c97a493d9d78c56fde39395f8531550da3277
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-03-31 20:44:25 +0200

    update benchmarks

--- runall-ec2.sh
+++ runall-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-AWS="ec2-52-41-173-74.us-west-2.compute.amazonaws.com"
+AWS="54.245.155.98"
 rsync -az  -e "ssh -i ~/Downloads/pair1.pem" --exclude=".git/*" . ec2-user@$AWS:~/ 
 
 ssh -i ~/Downloads/pair1.pem ec2-user@$AWS 'cd ~/advent-of-code && ./runall.sh'
--- runall.sh
+++ runall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 TOTAL=0.0
+CC=clang
 for dir in ./*/     
 do
     dir=${dir%*/} 
     dir=${dir#"./"}     
     cd $dir
-    cc *.c -Wall -Wextra -O2 -lm -march=native -o a.out
+    $CC *.c -Wall -Wextra -Ofast -lm -march=native -o a.out
     start_time="$(date -u +%s.%N)"
     ./a.out > /dev/null
     end_time="$(date -u +%s.%N)"

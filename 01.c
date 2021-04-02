f06fbc13fab18bf148e4e4e7f61d1269a938a3c2
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-04-02 16:28:27 +0200

    speed up day 1

--- 01.c
+++ 01.c
@@ -1,6 +1,19 @@
+#include <assert.h>
 #include <err.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int 
+intcmp(const void *p1, const void *p2) {
+  int32_t a = *(int32_t *) p1;
+  int32_t b = *(int32_t *) p2;
+  if (a == b) {
+    return 0;
+  }
+
+  return a < b ? -1 : 1;
+}
 
 int day1() {
   FILE* f = fopen("inputs/01.input", "r");
@@ -20,15 +33,24 @@ int day1() {
   }
   fclose(f);
 
+  // Since the 3 numbers need to sum to 2020 and most input numbers are over half that
+  // We're likely to find out answer sooner on a sorted list (small to large).
+  qsort(numbers, n, sizeof(int32_t), intcmp);
+
+  int32_t answer = 0;
   for (int32_t i = 0; i < n; i++) {
     for (int32_t j = i; j < n; j++) {
       for (int32_t k = j; k < n; k++) {
         if (numbers[i] + numbers[j] + numbers[k] == 2020) {
-          printf("%d\n", numbers[i] * numbers[j] * numbers[k]);
-          return 0;
+          answer = numbers[i] * numbers[j] * numbers[k];
+          goto PRINT_RESULT;
         }
       }
     }
   }
+
+PRINT_RESULT: ;
+  printf("%d\n", answer);
+  assert(answer == 157667328);
   return 0;
 }

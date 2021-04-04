30a1dcb609306b8ef80356b9f8572e00421e65fe
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-04-04 15:37:53 +0200

    add script for creating profile guided optimized binary

--- run-on-ec2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e 
-AWS="54.244.68.210"
-rsync -az  -e "ssh -i ~/Downloads/pair1.pem" --exclude=".git/*" . ec2-user@$AWS:~/advent-of-code 
-ssh -i ~/Downloads/pair1.pem ec2-user@$AWS 'cd ~/advent-of-code && make release && ./aoc2020'

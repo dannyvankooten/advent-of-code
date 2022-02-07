ff845d8969148a06d7b6de9040b92d1f017f470a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-02-07 10:50:09 +0100

    day 6

---
+++ 06-lanternfish/main.py
@@ -0,0 +1,26 @@
+input = open("input.txt").read()
+fish = [int(n) for n in input.split(",")]
+
+# create list of pregnancies so we know how many fish are bown on each cycle day
+pregnancies = [0] * 7
+for f in fish:
+    pregnancies[f] += 1
+
+# babies are fish that have yet to mature
+babies = [0] * 7
+
+num_fish = len(fish)
+for d in range(0, 256):
+    day_in_cycle = d % 7
+
+    # a new fish is born
+    num_fish += pregnancies[day_in_cycle]
+
+    # this fish will start producing babies after 9 days
+    babies[(d+2) % 7] += pregnancies[day_in_cycle]
+
+    pregnancies[day_in_cycle] += babies[day_in_cycle]
+    babies[day_in_cycle] = 0
+   
+
+print("Amount of fish: ", num_fish)

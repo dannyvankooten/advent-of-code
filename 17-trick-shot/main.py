0ba215ece55a754e463f49524c9f9694f50ac0f2
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-09-25 16:45:59 +0200

    optimize day 17 a little - there's way more to optimise though

--- 17-trick-shot/main.py
+++ 17-trick-shot/main.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import sys 
 import parse 
 from dataclasses import dataclass 
-
+import math 
 def parse_input(input):
     pattern = "target area: x={x1:d}..{x2:d}, y={y1:d}..{y2:d}"
     match = parse.search(pattern, input)
@@ -10,11 +10,13 @@ def parse_input(input):
 
 @dataclass
 class Velocity:
+    __slots__ = ['x', 'y']
     x: int 
     y: int 
 
 @dataclass
 class Probe:
+    __slots__ = ['x', 'y', 'velocity']
     x: int 
     y: int 
     velocity: Velocity
@@ -32,22 +34,26 @@ class Probe:
 
 
 def solve(target):
-
-    x_dir = 1 if target['x1'] > 0 else -1
     distinct_velocity_values = 0
+    x_limit = max(abs(target['x1']), abs(target['x2'])) + 1
+    y_limit = max(abs(target['y1']), abs(target['y2'])) + 1
+    step_limit = 2 * y_limit
 
-    for x in range(x_dir, target['x2'] * 2, x_dir):
-        for y in range(-3 * abs(target['y2']), abs(target['y2'] * 3)):
+    for x in range(-x_limit, x_limit):
+        for y in range(-y_limit, y_limit):
             probe = Probe(0, 0, Velocity(x, y))
-            for step in range(target['x1'] * 5):
+            for _ in range(step_limit):
                 probe.move()
 
                 if target['x1'] <= probe.x <= target['x2'] and target['y1'] <= probe.y <= target['y2']:
                     distinct_velocity_values += 1
                     break
-                elif x_dir > 0 and probe.x > target['x2'] or x_dir < 0 and probe.x < target['x1']:
+                elif abs(probe.x) > max(abs(target['x1']), abs(target['x2'])):
                     # overshot
-                    break 
+                    break
+                elif probe.y < target['y1']:
+                    # gravity baby
+                    break
 
     return distinct_velocity_values
 
@@ -56,5 +62,4 @@ if __name__ == '__main__':
         with open(path) as f:
             input = f.read().strip()
             data = parse_input(input)
-            print(data)
             print(solve(data))

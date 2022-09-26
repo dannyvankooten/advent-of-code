55134f29e05af6a4ee41c26b813f7eed94617247
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-09-26 18:37:30 +0200

    day 12

--- 2021/12-passage-pathing/main.py
+++ 2021/12-passage-pathing/main.py
@@ -1,21 +1,36 @@
 from collections import defaultdict
+import sys 
 
-connect = defaultdict(list)
-for line in open("input.txt").readlines():
-    pair = line.strip().split('-')
-    for p1, p2 in zip(pair, reversed(pair)):
-        if p2 != 'start':
-            connect[p1].append(p2)
+def parse(input_file):
+    neighbors = defaultdict(list)
+
+    with open(input_file) as f:
+        for line in f.readlines():
+            a, b = line.strip().split('-')
+            neighbors[a] += [b]
+            neighbors[b] += [a]
+
+    return neighbors
     
-del connect['end']
+def solve(neighbors, part, seen = [], cave='start'):
+    if cave == 'end': 
+        return 1 
 
-def pt1(data, path):
-    final = 0
-    for point in data[path[-1]]:
-        if point.isupper() or not point in path:
-            final += 1 if point == 'end' else pt1(data, path + [point])
-    return final
+    if cave in seen:
+        if cave == 'start':
+            return 0
+        if cave.islower():
+            if part == 1: 
+                return 0
+            else:
+                part = 1
+
+    return sum(solve(neighbors, part, seen + [cave], n) for n in neighbors[cave])
 
 
-count = pt1(connect, ['start'])
-print(count)
+if __name__ == '__main__':
+    for f in sys.argv[1:]:
+        neighbors = parse(f)
+        print(neighbors)
+
+        print(solve(neighbors, part=1), solve(neighbors, part=2))

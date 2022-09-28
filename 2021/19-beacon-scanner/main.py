3299f5e116f07c2bf187f48c49dee7c98354570f
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-09-28 14:03:17 +0200

    minor cleanup day 19

--- 2021/19-beacon-scanner/main.py
+++ 2021/19-beacon-scanner/main.py
@@ -1,7 +1,5 @@
 from collections import defaultdict, namedtuple
-from dataclasses import dataclass
-from itertools import combinations, permutations, product 
-import numpy as np
+from itertools import permutations, product 
 
 Point = namedtuple("Point", ["x", "y", "z"])
 
@@ -10,15 +8,16 @@ class Scanner():
         self.index = i
         self.report = report 
         self.pos = Point(None, None, None)
-        self.rel_to = None
         self.fixed = False
 
         if i == 0:
             self.fixed = True
             self.pos = Point(0, 0, 0)
 
-    def report_permutations(self):
+    def create_permutations(self):
         new_coords = []
+
+        # Create all possible permutations... But why are there 48 (8*6) instead of 24?
         for (cx, cy, cz) in product([1, -1], repeat=3):
             new_coords.append([(x*cx, y*cy, z*cz) for (x, y, z) in self.report])
             new_coords.append([(x*cx, y*cy, z*cz) for (x, z, y) in self.report])
@@ -27,7 +26,7 @@ class Scanner():
             new_coords.append([(x*cx, y*cy, z*cz) for (z, x, y) in self.report])
             new_coords.append([(x*cx, y*cy, z*cz) for (z, y, x) in self.report])
 
-        return new_coords
+        self.report_permutations = new_coords
 
     def fix_orientation(self, report):
         self.report = report
@@ -35,9 +34,6 @@ class Scanner():
 
     def set_position(self, x, y, z):
         self.pos = Point(x, y, z)
-        #
-
-        # TODO: adjust report
     
 
 def solve(lines):
@@ -55,15 +51,20 @@ def solve(lines):
         (x, y, z) = [int(n) for n in l.split(",")]
         scanner.report.append((x, y, z))
 
-    while len(list(filter(lambda s: s.fixed is False, scanners))) > 0:
+    # create permutations
+    for s in scanners[1:]:
+        s.create_permutations()
+
+    # keep matching beacon positions until every scanner is fixed
+    while len(list(filter(lambda s: s.fixed is False, scanners[1:]))) > 0:
         for s1 in filter(lambda s: s.fixed, scanners):
             for s2 in filter(lambda s: not s.fixed, scanners):
-                for report in s2.report_permutations():
+                for report in s2.report_permutations:
                     positions = defaultdict(int)
                     for (x1, y1, z1) in s1.report:
                         for (x2, y2, z2) in report:
                             positions[((x2-x1), (y2-y1), (z2-z1))] += 1
-                
+                                          
                     positions = {k: v for k, v in positions.items() if v >= 12}
                     if positions:
                         s2.fix_orientation(report)
@@ -72,9 +73,9 @@ def solve(lines):
                         break
             
 
-    all = set([])
-    for s in scanners:
-        # adjust reports based on absolute position                    
+    all = set(scanners[0].report)
+    for s in scanners[1:]:
+        # adjust report based on absolute position                    
         s.report = map(lambda pos: (pos[0] + s.pos.x, pos[1] + s.pos.y, pos[2] + s.pos.z), s.report)
 
         # add beacons to set to count unique values
@@ -91,5 +92,6 @@ def solve(lines):
     print("Part 2: ", max_manhattan)
 
 
-with open(0) as f:
-    solve(f.readlines())
+if __name__ == '__main__':
+    with open(0) as f:
+        solve(f.readlines())
--- 2021/19-beacon-scanner/test.py
+++ 2021/19-beacon-scanner/test.py
@@ -4,8 +4,20 @@ from main import *
 
 class PuzzleTestCase(unittest.TestCase):
     
-    def test_runs(self):
-        self.assertTrue(True)
+    def test_permute(self):
+        new_values = []
+        values = [(1, 2, 3), (4, 5, 6)]
+        for (cx, cy, cz) in product([1, -1], repeat=3):
+            new_values.append([(x*cx, y*cy, z*cz) for (x, y, z) in values])
+            #new_values.append([(x*cx, y*cy, z*cz) for (x, z, y) in values])
+            new_values.append([(x*cx, y*cy, z*cz) for (y, z, x) in values])
+            #new_values.append([(x*cx, y*cy, z*cz) for (y, x, z) in values])
+            new_values.append([(x*cx, y*cy, z*cz) for (z, x, y) in values])
+            #new_values.append([(x*cx, y*cy, z*cz) for (z, y, x) in values])
+
+   
+        print(new_values)
+        self.assertEqual(len(new_values), 24)
         
 
 

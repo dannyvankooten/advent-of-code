4715fe29ff0d3d224f892eec01851e80fff36cf2
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-10 08:14:47 +0100

    (2022, python) golf day 10 a bit

--- 2022/10-cathode-ray-tube/main.py
+++ 2022/10-cathode-ray-tube/main.py
@@ -1,44 +1,44 @@
 from pathlib import Path
 
-def draw(cycle, x):
+def draw(cycle: int, x: int):
+    """draw CRT pixel"""
     n = cycle % 40
+    if n == 0:
+        print()
+
     if abs(x - n) <= 1:
         print('#', end='')
     else:
         print('.', end='')
 
     #print(f"cycle: {cycle}\tsprite: {x}\tprinting at position {n}")
-    if cycle > 0 and n == 0:
-        print()
 
-def signal_strength(cycle, x) -> int:
-    if cycle == 20 or (cycle - 20) % 40 == 0:
+def signal_strength(cycle: int, x: int) -> int:
+    """Returns signal strength of current cycle + register combo"""
+    if cycle == 20 or (cycle - 20) % 40 == 0 and cycle <= 220:
         return x * cycle 
 
     return 0
 
-def solve(input) -> int:
+
+def solve(input: list[str]) -> int:
     x = 1
     cycles = 0 
     signal_sum = 0
-    for line in input.strip().split('\n'):
-        line = line.split()
-        if line[0] == 'noop':
+    
+    for line in map(lambda l: l.split(), input):
+        n = 2 if line[0] == 'addx' else 1
+        for _ in range(0, n):
             draw(cycles, x)
             cycles += 1
             signal_sum += signal_strength(cycles, x)
-        elif line[0] == 'addx':
-            for _ in range(0, 2):
-                draw(cycles, x)
-                cycles += 1
-                signal_sum += signal_strength(cycles, x)
-
+        if n == 2:
             x += int(line[1])
 
     return signal_sum
 
 if __name__ == '__main__':
-    #input = Path("input_test.txt").read_text()
-    input = Path("input.txt").read_text()
-    print("pt1: ", solve(input))
+    input = Path("input.txt").read_text().strip().split('\n')
+
+    print("\npt1: ", solve(input))
 

2fadb6c5656d83831dad852693290934fe26b25b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-15 15:55:05 +0100

    2022/15 refactor pt1 to use same Sensor class as pt2

--- 2022/15-beacon-exclusion-zone/main.py
+++ 2022/15-beacon-exclusion-zone/main.py
@@ -1,71 +1,20 @@
 from pathlib import Path
-from dataclasses import dataclass
 
-@dataclass()
 class Point:
     __slots__ = ("x", "y")
-    x: int 
+    x: int
     y: int 
 
+    def __init__(self, x: int, y: int):
+        self.x = x 
+        self.y = y
 
-def manhattan_distance(a, b): 
-    return abs(a.x - b.x) + abs(a.y - b.y)
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y 
 
+    def __hash__(self):
+        return hash((self.x, self.y))
 
-def parse(input):
-    input = input.replace('Sensor at ', '').replace(': closest beacon is at', '').replace(',', '').replace('x=', '').replace('y=', '')
-    
-    sensors = []
-    for line in input.strip().split('\n'):
-        sx, sy, bx, by = map(int, line.split())
-        sensor = Point(sx, sy) 
-        beacon = Point(bx, by) 
-        sensors.append((sensor, beacon))
-
-    return sensors  
-
-def count_excluded_positions_on_y(sensors, search_y):
-    count = 0
-    ranges = []
-    exclusions = set()
-    
-    for s, b in sensors: 
-        dy = -1 if s.y > search_y else 1
-        top = Point(s.x, search_y)
-        treshold = manhattan_distance(s, b)
-        while manhattan_distance(s, top) < treshold:
-            top.y += dy 
-        
-        diff = abs(top.y - search_y) 
-        if diff == 0:
-            continue 
-        start = s.x - diff 
-        end = s.x + diff 
-        ranges.append((start, end)) 
-
-        if b.y == search_y:
-            exclusions.add(b.x)
-   
-    ranges.sort()
-    prev = ranges[0][0] - 1
-    for start, end in ranges:
-        if start <= prev:
-            start = prev + 1
-        
-        diff = end - start + 1
-        if diff > 0:
-            count += end - start + 1
-
-        if end > prev:
-            prev = end
-
-
-
-    count -= len(exclusions)
-    return count
-
-def mh(x1, x2, y1, y2):
-    return abs(x1 - x2) + abs(y1 - y2)
 
 class Sensor:
     __slots__ = ("x", "y", "size")
@@ -89,52 +38,107 @@ class Sensor:
     def __repr__(self):
         return f"Sensor (x={self.x}, y={self.y})"
     
-    def covers(self, px, py):
+    def covers(self, px: int , py: int):
         y_diff = abs(py - self.y) 
         x_size = self.size - y_diff 
         if x_size <= 0:
-            return False 
+            return 0 
 
         x_start = self.x - x_size 
         if x_start > px:
-            return False 
+            return 0 
 
         x_end = self.x + x_size 
         if x_end < px:
-            return False 
+            return 0 
 
         return x_end 
 
 
-def find_position(sensors, lim):
-    px, py = 0, 0
-    sensors = [Sensor(s, b) for s, b in sensors]
+def parse(input: str):
+    input = input.replace('Sensor at ', '').replace(': closest beacon is at', '').replace(',', '').replace('x=', '').replace('y=', '')
+    
+    sensors = []
+    beacons = set()
+    for line in input.strip().split('\n'):
+        sx, sy, bx, by = map(int, line.split())
+        sensor = Point(sx, sy) 
+        beacon = Point(bx, by) 
+        sensors.append(Sensor(sensor, beacon))
+        beacons.add(beacon)
 
+    assert(len(sensors) > len(beacons))
+
+    return sensors, list(beacons)
+
+def mh(x1: int, x2: int, y1: int, y2: int):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def count_excluded_positions_on_y(sensors: list[Sensor], beacons: list[Point], search_y: int) -> int:
+    count = 0
+    ranges = []
+
+    for s in sensors:
+        end = s.covers(s.x, search_y)
+        if end == 0:
+            continue 
+
+        start = s.x - (end - s.x)
+        ranges.append((start, end))
+  
+    # sum non-overlapping ranges
+    ranges.sort()
+    prev = ranges[0][0] - 1
+    for start, end in ranges:
+        if start <= prev:
+            start = prev + 1
+        
+        diff = end - start + 1
+        if diff > 0:
+            count += end - start + 1
+
+        if end > prev:
+            prev = end
+    
+    # exclude beacons on this Y
+    for b in beacons:
+        if b.y == search_y:
+            count -= 1 
+
+    return count
+
+def find_position(sensors: list[Sensor], lim: int) -> int:
+    px, py = 0, 0
     free_spot = False 
+
     while free_spot == False: 
         free_spot = True
 
         for s in sensors:
             next_x = s.covers(px, py) 
-            if next_x != False:
-                free_spot = False 
-                px = next_x + 1
-                if px >= lim:
-                    px = 0 
-                    py += 1
+            if next_x == 0:
+                continue 
 
-                # go to next x, y position    
-                break
-                
+            free_spot = False 
+
+            # go to next (x, y) position
+            px = next_x + 1
+            if px >= lim:
+                px = 0 
+                py += 1
+              
+
+            break
     return px * 4000000 + py
 
 
 if __name__ == '__main__':
     input = Path("input.txt").read_text()
-    sensors = parse(input) 
+    sensors, beacons = parse(input) 
 
     # pt1
-    pt1 = count_excluded_positions_on_y(sensors, 2000000) 
+    pt1 = count_excluded_positions_on_y(sensors, beacons, 2000000) 
     print("pt1: ", pt1)
     assert(pt1 == 5240818)
 

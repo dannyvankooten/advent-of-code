23461f3d2394b410706a7c331bac4f1a75b40e9c
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-03 19:26:31 +0100

    2015 day 5 part 2

--- 2015/05-nice-strings/main.c
+++ 2015/05-nice-strings/main.c
@@ -30,6 +30,36 @@ int str_is_nice(char *s) {
     return repeats > 0 && vowelcount >= 3 ? 1 : 0;
 }
 
+
+int str_is_nice_v2(char *s) {
+    int r1 = 0;
+    int r2 = 0;
+    while (*s != '\n' && *s != '\0') {
+
+        // check for rule 1
+        char *s2 = s;
+        if (r1 == 0 && *(s+1) != '\n' && *(s+2) != '\n' && *(s+2) != '\n' && *(s+2) != '\0') {
+            s2 += 2;
+            while (r1 == 0 && *s2 != '\n' && *s2 != '\0') {
+                if (memcmp(s, s2, 2) == 0) {
+                    r1 = 1;
+                }
+
+                s2++;
+            }
+        }
+
+        // check for rule 2
+        if (*(s+1) != '\n' && *(s+2) == *s) {
+            r2 = 1;
+        }
+
+        s++;
+    }
+
+    return r1 == 1 && r2 == 1;
+}
+
 int main() {
     FILE *fp = fopen("input.txt", "r");
     char input[20*1024];
@@ -38,16 +68,22 @@ int main() {
     fclose(fp);
 
     char *s = input;
-    int nice = 0;
+    int nice_v1 = 0;
+    int nice_v2 = 0;
     while (*s != '\0') {
         if (str_is_nice(s) == 1) {
-            nice++;
+            nice_v1++;
+        }
+
+         if (str_is_nice_v2(s) == 1) {
+            nice_v2++;
         }
 
         while (*s != '\n' && *s != '\0') s++;
         if (*s == '\n') s++;
     }
 
-    printf("%d\n", nice);
+    printf("%d\n", nice_v1);
+    printf("%d\n", nice_v2);
     return 0;
 }

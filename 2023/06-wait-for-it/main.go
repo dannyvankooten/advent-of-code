f4cd2974f24697e143b43db5455c2427fc912c2d
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-06 10:39:24 +0100

    [perf] 2023 day 6, use quadratic formula

--- 2023/06-wait-for-it/main.go
+++ 2023/06-wait-for-it/main.go
@@ -2,31 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
 func solve(times []int, distances []int) int {
+	// -x^2 + tx - d > 0
+	// x = (-t + sqrt(t^2 - 4 * -1 * -d ) / - 2
+	// x = (-t - sqrt(t^2 - 4 * -1 * -d ) / - 2
 	prod := 1
-	count := 0
-	tMoving := 0
-
-	for i, t := range times {
-		count = 0
-
-		start := t * 20 / 100
-		end := t * 80 / 100
-
-		for tCharging := start; tCharging <= end; tCharging++ {
-			tMoving = t - tCharging
-
-			if (tCharging * tMoving) > distances[i] {
-				count++
-			} else if count > 0 {
-				break
-			}
-		}
-
-		prod *= count
+	for i := 0; i < len(times); i++ {
+		t := float64(times[i])
+		d := float64(distances[i])
+		dis := math.Sqrt(math.Pow(t, 2) - 4*-1*-d)
+		x1 := (-t - dis) / -2
+		x2 := (-t + dis) / -2
+		prod *= int(x1) - int(x2)
 	}
 
 	return prod
@@ -38,8 +29,10 @@ func main() {
 	times := []int{61, 70, 90, 66}
 	distances := []int{643, 1184, 1362, 1041}
 
+	fmt.Print("---- Day 6: Wait For It ---\n")
+
 	a1 := solve(times, distances)
-	fmt.Printf("---- Day 6: Wait For It ---\npart 1: %d\n", a1)
+	fmt.Printf("part 1: %d\n", a1)
 
 	a2 := solve([]int{61709066}, []int{643118413621040})
 	fmt.Printf("part 2: %d\n", a2)

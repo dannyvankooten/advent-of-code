e8cca28a38faf2a50b9ee7354e07a7fa3458f48d
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-07 16:57:02 +0100

    2017 readme

--- 2017/01-inverse-captcha/main.c
+++ 2017/01-inverse-captcha/main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 
 int solve(const char *input, size_t size, int offset) {
     int sum = 0;
@@ -15,6 +15,9 @@ int solve(const char *input, size_t size, int offset) {
 }
 
 int main() {
+    clock_t start_t, end_t;
+    start_t = clock();
+
     FILE *fp = fopen("input.txt", "r");
     if (!fp) {
         abort();
@@ -22,8 +25,14 @@ int main() {
     char input[32*1024];
     size_t nread = fread(input, 1, 32*1024, fp);
     input[nread] = '\0';
+    fclose(fp);
 
-    printf("%d\n", solve(input, nread, 1));
-    printf("%d\n", solve(input, nread, (nread-1)/2));
+    printf("--- Day 1: Inverse Captcha ---\n");
+    printf("Part 1: %d\n", solve(input, nread, 1));
+    printf("Part 2: %d\n", solve(input, nread, (nread-1)/2));
+
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC * 1000;
+    printf("Time: %.2fms\n", total_t);
     return 0;
 }

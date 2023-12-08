194a25e00262ce44455b833e0bf79570080c6392
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-08 16:07:14 +0100

    2017 day 7 part 2

--- 2017/07-recursive-circus/main.c
+++ 2017/07-recursive-circus/main.c
@@ -93,14 +93,59 @@ unsigned char isroot(program_t *node, program_t *programs, int nprograms) {
     return 1;
 }
 
-char *pt1(program_t *programs, int nprograms) {
-    for (int i = 0; i < nprograms; i++) {
-        if (isroot(&programs[i], programs, nprograms)) {
-            return programs[i].name;
+program_t *byname(char *name, program_t *programs, int nprograms) {
+    for (int j = 0; j < nprograms; j++) {
+        if (strcmp(name, programs[j].name) == 0) {
+            return &programs[j];
         }
     }
 
     return NULL;
+}
+
+int sumweight(program_t *root, program_t *programs, int nprograms) {
+    int sum = root->weight;
+    for (int i = 0; i < root->nchildren; i++) {
+        program_t *child = byname(root->children[i], programs, nprograms);
+        sum += sumweight(child, programs, nprograms);
+    }
+    return sum;
+}
+
+program_t *pt1(program_t *programs, int nprograms) {
+    for (int i = 0; i < nprograms; i++) {
+        if (isroot(&programs[i], programs, nprograms)) {
+            return &programs[i];
+        }
+    }
+
+    return NULL;
+}
+
+int pt2(program_t *programs, int nprograms, program_t *root, int unbalanced) {
+    program_t *children[root->nchildren];
+    int weights[root->nchildren];
+
+    for (int i = 0; i < root->nchildren; i++) {
+        children[i] = byname(root->children[i], programs, nprograms);
+        weights[i] = sumweight(children[i], programs, nprograms);
+    }
+
+    for (int i = 0, diff = 0; i < root->nchildren; i++) {
+        for (int j = 0; j < root->nchildren; j++) {
+            if (i == j) continue;
+            diff = weights[i] - weights[j];
+            if (diff == 0) {
+                break;
+            }
+        }
+
+        if (diff != 0) {
+            return pt2(programs, nprograms, children[i], diff);
+        }
+    }
+
+    return root->weight - unbalanced;
 }
 
 int main() {
@@ -108,15 +153,19 @@ int main() {
     start_t = clock();
 
     char input[1024 * 64];
-    read_input_file(input, "input_test.txt");
+    read_input_file(input, "input.txt");
 
     program_t *programs = malloc(2048 * sizeof(program_t));
     int nprograms = parse_input(programs, input);
+    program_t *root = pt1(programs, nprograms);
+
+    int a2 = pt2(programs, nprograms, root, 0);
 
     printf("--- %s ---\n", PUZZLE_NAME);
-    printf("Part 1: %s\n", pt1(programs, nprograms));
-    printf("Part 2: %d\n", 0);
+    printf("Part 1: %s\n", root->name);
+    printf("Part 2: %d\n", a2);
 
+    free(programs);
     end_t = clock();
     double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC * 1000;
     printf("Time: %.2fms\n", total_t);

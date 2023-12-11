13d595b0cfdfe60b3160139109e0d064eda70b5e
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-11 14:04:10 +0100

    2018 day 4

--- 2018/04-repose-record/main.c
+++ 2018/04-repose-record/main.c
@@ -16,6 +16,12 @@ struct log_entry {
     int awake;
 };
 
+struct guard {
+    int id;
+    int minutes[60];
+    int total_sleep_time;
+};
+
 static int log_entry_compare(const void *p1, const void *p2) {
     struct log_entry *a = (struct log_entry *) p1;
     struct log_entry *b = (struct log_entry *) p2;
@@ -74,59 +80,101 @@ int parse(struct log_entry *log_entries, char *s) {
         while (*s != '\n' && *s != 0x0) s++;
         if (*s == '\n') s++;
     }
+
+    qsort(log_entries, nentries, sizeof(struct log_entry), log_entry_compare);
     return nentries;
 }
 
-int solve(struct log_entry *entries, int nentries) {
-    int sleepcount[100];
-    memset(sleepcount, 0, 100 * sizeof(int));
+struct guard* guard_by_id(struct guard *guards, int nguards, int id) {
+    for (int i = 0; i < nguards; i++) {
+        if (guards[i].id == id) {
+            return &guards[i];
+        }
+    }
 
-    int guard;
+    return NULL;
+}
+
+int log_entries_into_guards(struct guard *guards, struct log_entry *entries, int nentries) {
+    int nguards = 0;
+    struct guard *guard;
+
     for (int i=0; i < nentries; i++) {
         struct log_entry e = entries[i];
 
         if (e.guard_id > 0) {
-            printf("%d starts shift\n", e.guard_id);
-            guard = e.guard_id;
+            guard = guard_by_id(guards, nguards, e.guard_id);
+            if (guard == NULL) {
+                guard = &guards[nguards++];
+                guard->id = e.guard_id;
+                guard->total_sleep_time = 0;
+                memset(guard->minutes, 0, 60 * sizeof(*guard->minutes));
+            }
         } else if (e.awake > 0) {
-            printf("Guard %d wakes up, adding %d minutes\n", guard, e.minute - entries[i-1].minute);
-            sleepcount[guard] += e.minute - entries[i-1].minute;
+            struct log_entry  prev = entries[i-1];
+            guard->total_sleep_time += (e.minute - prev.minute);
+            for (int m = prev.minute; m < e.minute; m++) {
+                guard->minutes[m]++;
+            }
+        }
+    }
+    return nguards;
+}
+
+int solve_pt1(struct guard *guards, int nguards) {
+   struct guard *guard = &guards[0];
+
+   for (int i = 0; i < nguards; i++) {
+        if (guards[i].total_sleep_time > guard->total_sleep_time) {
+            guard = &guards[i];
         }
     }
 
-    int max_idx = -1;
-    int max_value = 0;
-    for (int i = 0; i < 100; i++) {
-        if (sleepcount[i] > max_value) {
-            max_value = sleepcount[i];
-            max_idx = i;
-        }
+    int sleepy_minute = 0;
+    for (int m = 0; m < 60; m++) {
+        if (guard->minutes[m] > guard->minutes[sleepy_minute]) {
+            sleepy_minute = m;
+       }
     }
 
-    printf("Guard %d sleeps the most: %d minutes\n", max_idx, max_value);
+    int answer_pt1 = guard->id * sleepy_minute;
+    return answer_pt1;
+}
+
+int solve_pt2(struct guard *guards, int nguards) {
+   struct guard *guard = &guards[0];
+   int max = 0;
+
+   for (int i = 0; i < nguards; i++) {
+       for (int m = 0; m < 60; m++) {
+           if (guards[i].minutes[m] > guard->minutes[max]) {
+               guard = &guards[i];
+               max = m;
+           }
+       }
+    }
+
+    return guard->id * max;
 }
 
 int main() {
     clock_t t = timer_start();
-    char input[1024 * 64];
-    read_input_file(input, "input_test.txt");
+    char input[1024 * 64] = "";
+    read_input_file(input, "input.txt");
 
     struct log_entry *log_entries = malloc(2048 * sizeof (struct log_entry));
     int nentries = parse(log_entries, input);
 
-    printf("Scanned %d entries\n", nentries);
-    qsort(log_entries, nentries, sizeof(struct log_entry), log_entry_compare);
-    for (int i =0; i < nentries; i++) {
-        struct log_entry *e = &log_entries[i];
-        printf("[%04d-%02d-%02d %02d:%02d] guard:%d awake:%d\n", e->year, e->month, e->day, e->hour, e->minute, e->guard_id, e->awake);
-    }
-
-    int a1 = solve(log_entries, nentries);
+    struct guard *guards = malloc(512 * sizeof(struct guard));
+    int nguards = log_entries_into_guards(guards, log_entries, nentries);
+    int pt1 = solve_pt1(guards, nguards);
+    int pt2 = solve_pt2(guards, nguards);
 
     printf("--- %s ---\n", PUZZLE_NAME);
-    printf("Part 1: %d\n", a1);
-    printf("Part 2: %d\n", 0);
+    printf("Part 1: %d\n", pt1);
+    printf("Part 2: %d\n", pt2);
     printf("Time: %.2fms\n", timer_stop(t));
+    free(guards);
     free(log_entries);
     return EXIT_SUCCESS;
 }

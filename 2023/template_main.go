62b1663d962c4c07e23f29255fa9be823944b646
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-11 09:59:58 +0100

    2023 day 11

--- 2023/template_main.go
+++ 2023/template_main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	timeStart := time.Now()
+
 	input, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	log.Printf("Input: %s\n", input)
+
+	pt1 := 0
+	pt2 := 0
+	fmt.Printf("Part 1: %d\n", pt1)
+	fmt.Printf("Part 2: %d\n", pt2)
+	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
 }

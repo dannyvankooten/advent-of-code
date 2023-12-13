c3ce22e3292e7df0833e393041da704fa0a93718
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-13 16:49:21 +0100

    2015 day 12

--- 2015/12-jsabacusframework-io/main.c
+++ 2015/12-jsabacusframework-io/main.c
@@ -1,136 +1,104 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "../adventofcode.h"
 #include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <stdbool.h>
-#include <stdint.h>
 
-static inline
-char *parse_int(int *dst, char *s) {
-    int n = 0;
-    int mod = 1;
-
-    if (*s == '-') {
-        mod = -1;
-        s++;
+int pt1(const char *input) {
+  int sum = 0;
+  const char *s = input;
+  int n;
+  while (*s != '\0') {
+    // since there are no strings
+    // simply parse everything that looks like a number
+    if (*s == '-' || (*s >= '0' && *s <= '9')) {
+      s = parse_int(&n, s);
+      sum += n;
     }
 
-    while (*s >= '0' && *s <= '9') {
-        n = (n * 10) + (*s - '0');
-        s++;
-    }
-
-    n *= mod;
-    *dst = n;
-
-    return s;
+    // skip everything else
+    s++;
+  }
+  return sum;
 }
 
-int pt1(char *input) {
-    int sum = 0;
-    char *s = input;
-    int n;
-    while (*s != '\0') {
-        if (*s == '-' || (*s >= '0' && *s <= '9')) {
-            s = parse_int(&n, s);
-            sum += n;
-        }
+int pt2(const char *input) {
+  const char *s = input;
+  int n;
 
-        s++;
+  const int MAX_DEPTH = 64;
+  int objsums[MAX_DEPTH];
+  memset(objsums, 0, MAX_DEPTH * sizeof(int));
+  objsums[0] = 0;
+  int objs = 0;
+  int red = 0;
+  while (*s != '\0') {
+
+    // only add to sum if not in "red" object
+    if ((*s == '-' || (*s >= '0' && *s <= '9'))) {
+      s = parse_int(&n, s);
+      objsums[objs] += n;
     }
-    return sum;
-}
 
-int pt2(char *input) {
-    int sum = 0;
-    char *s = input;
-    int n;
+    switch (*s) {
+    case '{':
+      objs++;
+      objsums[objs] = 0;
+      break;
 
-    int objsums[64];
-    memset(objsums, 0, 64 * sizeof(int));
-    objsums[0] = 0;
-    int objs = 0;
-    int red = 0;
-    while (*s != '\0') {
+    case '}':
+      // check if we broke out of red objecty
+      if (red == 0) {
+        objsums[objs - 1] += objsums[objs];
+      } else if (objs == red) {
+        red = 0;
+      }
 
-        // only add to sum if not in "red" object
-        if (red == 0 && (*s == '-' || (*s >= '0' && *s <= '9'))) {
-            s = parse_int(&n, s);
-            sum += n;
-            objsums[objs] += n;
-        }
+      objs--;
+      break;
 
-        switch (*s) {
-            case '{':
-                objs++;
-                objsums[objs] = 0;
-                break;
-
-            case '}':
-                // check if we broke out of red objecty
-                if (red == objs) {
-                    red = 0;
-                }
-                objs--;
-                break;
-
-            case ':':
-                // if property has value of "red"
-                // mark current object as red
-                // and substract everything added to sum within this object
-                // or any of its children
-                if (red == 0 && strncmp(s, ":\"red\"", 6) == 0) {
-                    red = objs;
-
-                    for (int i=63; i >= objs; i--) {
-                        sum -= objsums[i];
-                        objsums[i] = 0;
-                    }
-                }
-                break;
-        }
-
-
-        s++;
+    case ':':
+      // if property has value of "red"
+      // mark current object as red
+      // and substract everything added to sum within this object
+      // or any of its children
+      if (red == 0 && strncmp(s, ":\"red\"", 6) == 0) {
+        red = objs;
+      }
+      break;
     }
-    return sum;
+
+    s++;
+  }
+
+  return objsums[0];
 }
 
 int main() {
-    FILE *fp = fopen("input.txt", "r");
-    if (fp == NULL) {
-        fprintf(stderr, "error reading input.txt");
-        exit(EXIT_FAILURE);
-    }
-    char input[32*1024] = "";
-    size_t nread = fread(input, 1, 32*1024, fp);
-    fclose(fp);
-    input[nread] = '\0';
+  clock_t tstart = timer_start();
+  char input[32 * 1024] = "";
+  read_input_file(input, 1024 * 32, "input.txt");
 
-    clock_t start_t, end_t;
-    start_t = clock();
+  int a1 = 0, a2 = 0;
+  a1 = pt1(input);
+  a2 = pt2(input);
 
-    printf("--- Day 12: JSAbacusFramework.io ---\n");
-    int a1 = pt1(input);
-    printf("Part 1: %d\n", a1);
-    assert(a1 == 119433);
+  // aoc samples
+  assert(pt2("[1,2,3]") == 6);
+  assert(pt2("[1,{\"c\":\"red\",\"b\":2},3]") == 4);
+  assert(pt2("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}") == 0);
+  assert(pt2("[1,\"red\",5]") == 6);
+  assert(pt2("[1,{\"a\":10,\"c\":\"red\",\"b\":2},3]") == 4);
+  assert(pt2("[1,{\"c\":\"red\",\"b\":2},3]") == 4);
+  assert(pt2("[{\"a\":5,\"color\":\"red\", \"b\": 4}]") == 0);
+  assert(pt2("[{\"a\":5,\"b\":\"red\",\"c\":{\"a\": 5}}]") == 0);
+  assert(pt2("[{\"a\":5},{\"a\":5,\"b\":\"red\",\"c\":{\"a\": 5}}]") == 5);
+  assert(pt2("{\"a\":{\"a\":[1,2,3]}, \"b\":\"red\"}]") == 0);
 
-    assert(pt2("[1,{\"a\": 10, \"c\":\"red\",\"b\":2},3]") == 4);
-    assert(pt2("[1,{\"c\":\"red\",\"b\":2},3]") == 4);
-    assert(pt2("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}") == 0);
-    assert(pt2("[{\"a\":5,\"color\":\"red\", \"b\": 4}]") == 0);
-    assert(pt2("[{\"a\":5,\"b\":\"red\",\"c\":{\"a\": 5}}]") == 0);
-    assert(pt2("[{\"a\":5},{\"a\":5,\"b\":\"red\",\"c\":{\"a\": 5}}]") == 5);
-    assert(pt2("{\"a\":{\"a\":[1,2,3]}, \"b\":\"red\"}]") == 0);
-    assert(pt2("[1,\"red\",5]") == 6);
-    assert(pt2("[1,2,3]") == 6);
-
-    int a2 = pt2(input);
-    printf("Part 2: %d\n", a2);
-
-    end_t = clock();
-    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC * 1000;
-    printf("Time: %.2fms\n", total_t);
-    return 0;
+  printf("--- Day 12: JSAbacusFramework.io ---\n");
+  printf("Part 1: %d\n", a1);
+  printf("Part 2: %d\n", a2);
+  printf("Time: %.2fms\n", timer_stop(tstart));
+  return 0;
 }

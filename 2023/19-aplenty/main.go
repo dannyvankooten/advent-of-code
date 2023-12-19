72503149fff3be55324683be85e14d5cc43482ab
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-19 09:31:53 +0100

    2023 day 19 part 1

---
+++ 2023/19-aplenty/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Part map[rune]int
+
+type Workflow []Rule
+
+type Rule struct {
+	category    rune
+	operator    rune
+	right       int
+	consequence string
+}
+
+func parse(input []byte) (map[string]Workflow, []Part) {
+	sections := strings.Split(string(input), "\n\n")
+
+	// parse workflows
+	workflows := make(map[string]Workflow)
+	for _, line := range strings.Split(sections[0], "\n") {
+		pos := strings.Index(line, "{")
+		name := line[0:pos]
+		line = line[pos+1 : len(line)-1]
+
+		w := make(Workflow, 0, 4)
+		for _, rule := range strings.Split(line, ",") {
+			r := Rule{}
+			pos = strings.Index(rule, ":")
+			if pos > -1 {
+				r.category = rune(rule[0])
+				r.operator = rune(rule[1])
+				r.right, _ = strconv.Atoi(rule[2:pos])
+				r.consequence = rule[pos+1:]
+			} else {
+				r.consequence = rule
+			}
+			w = append(w, r)
+		}
+
+		workflows[name] = w
+	}
+
+	// parse parts
+	parts := make([]Part, 0)
+	for _, line := range strings.Split(sections[1], "\n") {
+		var x, m, a, s int
+		fmt.Sscanf(line, "{x=%d,m=%d,a=%d,s=%d}", &x, &m, &a, &s)
+		parts = append(parts, Part{
+			'x': x,
+			'm': m,
+			'a': a,
+			's': s,
+		})
+	}
+
+	return workflows, parts
+}
+
+func applyWorkflow(workflows map[string]Workflow, workflow string, part Part) bool {
+	for _, r := range workflows[workflow] {
+		var result bool
+
+		switch r.operator {
+		case '>':
+			result = part[r.category] > r.right
+			break
+		case '<':
+			result = part[r.category] < r.right
+			break
+		default:
+			result = true
+			break
+		}
+
+		// keep processing rules if expression evaluated to false
+		if !result {
+			continue
+		}
+
+		if r.consequence == "R" {
+			return false
+		} else if r.consequence == "A" {
+			return true
+		} else {
+			return applyWorkflow(workflows, r.consequence, part)
+		}
+	}
+
+	return false
+}
+
+func p1(workflows map[string]Workflow, parts []Part) int {
+	sum := 0
+	for _, p := range parts {
+		if applyWorkflow(workflows, "in", p) {
+			for _, v := range p {
+				sum += v
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	timeStart := time.Now()
+
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	workflows, parts := parse(input)
+
+	pt1 := p1(workflows, parts)
+	pt2 := 0
+	fmt.Printf("--- Day 19: Aplenty ---\n")
+	fmt.Printf("Part 1: %d\n", pt1)
+	fmt.Printf("Part 2: %d\n", pt2)
+	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
+}

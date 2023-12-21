81ad45a63ba63631285c254fcf793cd6a3b3d50a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-21 11:52:54 +0100

    2023 day 21 part 2

--- 2023/21-step-counter/main.go
+++ 2023/21-step-counter/main.go
@@ -8,23 +8,15 @@ import (
 	"time"
 )
 
-var plots = make(map[[2]int]bool)
 var seen = make(map[[3]int]bool)
+var options []int = make([]int, 65+131*2)
 
-func countSteps(grid []string, x int, y int, depth int, maxDepth int) int {
-	// no need to recurse further if we've already been here
-	key := [3]int{x, y, depth}
-	if _, ok := seen[key]; ok {
-		return 0
-	}
-
+func countStepsInfinite(grid []string, x int, y int, depth int, maxDepth int) {
 	if depth == maxDepth {
-		plots[[2]int{x, y}] = true
-		return 1
+		return
 	}
 
-	seen[key] = true
-	count := 0
+	n := len(grid)
 	for _, d := range [][]int{
 		{0, -1}, // N
 		{1, 0},  // E
@@ -34,18 +26,21 @@ func countSteps(grid []string, x int, y int, depth int, maxDepth int) int {
 		x2 := x + d[0]
 		y2 := y + d[1]
 
-		if x2 < 0 || y2 < 0 || y2 >= len(grid) || x2 >= len(grid[y2]) {
+		// modulo like Python
+		if grid[((y2%n)+n)%n][((x2%n)+n)%n] == '#' {
 			continue
 		}
 
-		if grid[y2][x2] == '#' {
+		key := [3]int{x2, y2, depth}
+		if _, ok := seen[key]; ok {
 			continue
 		}
+		seen[key] = true
 
-		count += countSteps(grid, x2, y2, depth+1, maxDepth)
+		options[depth] += 1
+		countStepsInfinite(grid, x2, y2, depth+1, maxDepth)
 	}
 
-	return count
 }
 
 func findStart(grid []string) (int, int) {
@@ -60,6 +55,14 @@ func findStart(grid []string) (int, int) {
 	return -1, -1
 }
 
+func f(x int, a [3]int) int {
+	b0 := a[0]
+	b1 := a[1] - a[0]
+	b2 := a[2] - a[1]
+
+	return b0 + (b1 * x) + (x*(x-1)/2)*(b2-b1)
+}
+
 func main() {
 	timeStart := time.Now()
 
@@ -70,15 +73,12 @@ func main() {
 	grid := strings.Split(strings.TrimSpace(string(input)), "\n")
 
 	x, y := findStart(grid)
-	countSteps(grid, x, y, 0, 64)
-	pt1 := len(plots)
+	countStepsInfinite(grid, x, y, 0, 65+131*2)
+	pt1 := options[63]
+	pt2 := f(26501365/131, [3]int{options[64], options[64+131], options[64+131*2]})
 
-	pt2 := 0
 	fmt.Printf("--- Day 21: Step Counter ---\n")
 	fmt.Printf("Part 1: %d\n", pt1)
 	fmt.Printf("Part 2: %d\n", pt2)
 	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
 }
-
-// 1202 too low
-// 1203 too low

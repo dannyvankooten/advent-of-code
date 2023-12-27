2f430dfe1972be6db9da3c708d8d6422bed5c485
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-27 20:14:40 +0100

    update readme

--- 2023/23-a-long-walk/main.go
+++ 2023/23-a-long-walk/main.go
@@ -15,6 +15,13 @@ var directions = [][2]int{
 	{0, -1}, // NORTH
 }
 
+type Node struct {
+	children []int
+	steps    []int
+	end      bool
+	x, y     int
+}
+
 func copyGrid(grid [][]rune) [][]rune {
 	c := make([][]rune, len(grid))
 	for i := range grid {
@@ -24,10 +31,32 @@ func copyGrid(grid [][]rune) [][]rune {
 	return c
 }
 
-func pt1(grid [][]rune, x int, y int, steps int) int {
-	var availableDirections [][2]int
+func key(x int, y int) int {
+	return y*150 + x
+}
+
+func createGraph(grid [][]rune, nodes map[int]Node, x int, y int) bool {
+	availableDirections := directions
+	steps := 0
+
+	parentKey := key(x, y)
+	parent, _ := nodes[parentKey]
+
+	prevX, prevY := 0, 0
 
 	for {
+		// if at end node
+		if y == len(grid)-1 && x == len(grid[y])-2 {
+			k := key(x, y)
+			if _, seen := nodes[k]; !seen {
+				nodes[k] = Node{end: true, x: x, y: y}
+			}
+			parent.children = append(parent.children, k)
+			parent.steps = append(parent.steps, steps)
+			nodes[parentKey] = parent
+			return true
+		}
+
 		switch grid[y][x] {
 		case '.':
 			availableDirections = directions
@@ -46,9 +75,6 @@ func pt1(grid [][]rune, x int, y int, steps int) int {
 			break
 		}
 
-		// mark tile as visited
-		grid[y][x] = 'O'
-
 		options := make([][2]int, 0, 4)
 		for _, d := range availableDirections {
 			x2 := x + d[0]
@@ -57,7 +83,12 @@ func pt1(grid [][]rune, x int, y int, steps int) int {
 				continue
 			}
 
-			if grid[y2][x2] == '#' || grid[y2][x2] == 'O' {
+			// don't walk back in direction we came from
+			if x2 == prevX && y2 == prevY {
+				continue
+			}
+
+			if grid[y2][x2] == '#' {
 				continue
 			}
 
@@ -65,28 +96,61 @@ func pt1(grid [][]rune, x int, y int, steps int) int {
 		}
 
 		if len(options) == 0 {
-			return steps
+			return false
 		} else if len(options) > 1 {
-			longest := 0
-			for _, coords := range options {
-				s := pt1(copyGrid(grid), coords[0], coords[1], steps+1)
-				if s > longest {
-					longest = s
+			// at junction, parse recursively
+			for _, c := range options {
+				k := key(c[0], c[1])
+				if _, seen := nodes[k]; !seen {
+					nodes[k] = Node{x: c[0], y: c[1]}
+					createGraph(grid, nodes, c[0], c[1])
 				}
+				parent.children = append(parent.children, k)
+				parent.steps = append(parent.steps, steps+1)
+				nodes[parentKey] = parent
 			}
 
-			return longest
+			return len(parent.children) > 0
+		} else {
+			prevX = x
+			prevY = y
+			x = options[0][0]
+			y = options[0][1]
+			steps += 1
 		}
-
-		x = options[0][0]
-		y = options[0][1]
-		steps += 1
 	}
 
 }
 
+func pt1(nodes map[int]Node, n Node, steps int, visited map[int]bool) int {
+	if n.end {
+		return steps
+	}
+
+	// mark node as visited
+	visited[key(n.x, n.y)] = true
+
+	longest := 0
+	for i := range n.children {
+		k := n.children[i]
+		child := nodes[k]
+		if v, ok := visited[k]; ok && v {
+			continue
+		}
+
+		s := pt1(nodes, child, steps+n.steps[i], visited)
+		if s > longest {
+			longest = s
+		}
+
+		visited[k] = false
+	}
+
+	return longest
+}
+
 func parse(input []byte) [][]rune {
-	grid := make([][]rune, 0)
+	grid := make([][]rune, 0, 141)
 	for _, l := range strings.Split(strings.TrimSpace(string(input)), "\n") {
 		grid = append(grid, []rune(l))
 	}
@@ -96,16 +160,22 @@ func parse(input []byte) [][]rune {
 func main() {
 	timeStart := time.Now()
 
-	input, err := os.ReadFile("input.txt")
+	input, err := os.ReadFile("input_test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	grid := parse(input)
+	nodes := map[int]Node{
+		key(1, 0): {x: 1, y: 0},
+	}
+	createGraph(grid, nodes, 1, 0)
+	fmt.Printf("%#v\n", nodes)
+	fmt.Printf("Start: %#v\n", nodes[key(1, 0)])
 
-	pt1 := pt1(grid, 1, 0, 0)
-
+	pt1 := pt1(nodes, nodes[key(1, 0)], 0, make(map[int]bool))
 	pt2 := 0
+
 	fmt.Printf("--- Day 23: A Long Walk ---\n")
 	fmt.Printf("Part 1: %d\n", pt1)
 	fmt.Printf("Part 2: %d\n", pt2)
--- 2023/go.mod
+++ 2023/go.mod
@@ -1,3 +1,13 @@
 module github.com/dannyvankooten/advent-of-code
 
 go 1.21.4
+
+require (
+	github.com/gammazero/deque v0.2.1
+	github.com/mitchellh/go-z3 v0.0.0-20191228203228-4cbedeba863f
+)
+
+require (
+	github.com/edwingeng/deque v1.0.3 // indirect
+	github.com/edwingeng/deque/v2 v2.1.1 // indirect
+)

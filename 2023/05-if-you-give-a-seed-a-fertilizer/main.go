2162c4e1e1c3259f33bf3ae7a356cb41218270b7
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-12-29 21:26:14 +0100

    attempt at speeding up 2023 day 5

--- 2023/05-if-you-give-a-seed-a-fertilizer/main.go
+++ 2023/05-if-you-give-a-seed-a-fertilizer/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 	"time"
 )
 
 func parseNumbers(s string) []int {
+	var err error
+
 	svalues := strings.Split(s, " ")
 	numbers := make([]int, len(svalues))
 	for i, s := range svalues {
-		numbers[i], _ = strconv.Atoi(s)
+		numbers[i], err = strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
 	}
 	return numbers
 }
@@ -28,20 +32,8 @@ func transform(source int, rules [][]int) int {
 	return source
 }
 
-func reverse(dest int, rules [][]int) int {
-	var source int
-	for _, rule := range rules {
-		source = dest + rule[1] - rule[0]
-		if source >= rule[1] && source < rule[1]+rule[2] {
-			return source
-		}
-	}
-
-	return dest
-}
-
-func parseInput() ([]int, [][][]int) {
-	bytes, err := os.ReadFile("input.txt")
+func parseInput(filename string) ([]int, [][][]int) {
+	bytes, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -50,14 +42,18 @@ func parseInput() ([]int, [][][]int) {
 	seeds := parseNumbers(lines[0][strings.Index(lines[0], ": ")+2:])
 	s := 0
 
-	translators := make([][][]int, 0, 8)
-	translators = append(translators, make([][]int, 0, 8))
+	translators := make([][][]int, 0, 7)
+	translators = append(translators, make([][]int, 0, 7))
 	for i := 3; i < len(lines); i++ {
 		// on every empty line, start a new map
 		if lines[i] == "" {
 			i += 1
 			s += 1
-			translators = append(translators, make([][]int, 0, 8))
+
+			if s == 7 {
+				break
+			}
+			translators = append(translators, make([][]int, 0, 7))
 			continue
 		}
 
@@ -82,35 +78,96 @@ func pt1(seeds []int, translators [][][]int) int {
 	return pt1
 }
 
-func pt2(seeds []int, translators [][][]int) int {
-	var s int
-	var l int
+var names = []string{
+	"seed-to-soil",
+	"soil-to-fertilizer",
+	"fertilizer-to-water",
+	"water-to-light",
+	"light-to-temperature",
+	"temperature-to-humidity",
+	"humidity-to-location",
+}
 
-	slices.Reverse(translators)
+type Range [2]int
 
-	for {
-		s = l
-		for _, t := range translators {
-			s = reverse(s, t)
+func transformRange(ranges []Range, rules [][]int) []Range {
+	// for every range, check if it's inside mapping
+	out := make([]Range, 0, len(ranges))
+
+	for _, r := range ranges {
+		mapped := false
+
+		for _, rule := range rules {
+			ruleStart := rule[1]
+			ruleEnd := rule[1] + rule[2] - 1
+			ruleOffset := rule[0] - rule[1]
+
+			// if entirely before or after rule, skip rule entirely
+			if r[1] < ruleStart || r[0] > ruleEnd {
+				continue
+			}
+
+			// range is at least partially inside this rule's mapping
+			// create a before, inside and after split
+
+			mapped = true
+
+			if r[0] < ruleStart {
+				before := Range{r[0], ruleStart - 1}
+				out = append(out, before)
+			}
+			inside := Range{
+				max(r[0], ruleStart) + ruleOffset,
+				min(r[1], ruleEnd) + ruleOffset,
+			}
+			out = append(out, inside)
+
+			if r[1] > ruleEnd {
+				after := Range{ruleEnd + 1, r[1]}
+				out = append(out, after)
+			}
+
+			// range was mapped, do not process further rules... right?
+			break
 		}
 
-		// check if s is valid seed number
-		for i := 0; i < len(seeds); i += 2 {
-			if s > seeds[i] && s < seeds[i]+seeds[i+1] {
-				return l
+		// if not mapped to any rule
+		// copy range as is
+		if !mapped {
+			out = append(out, r)
+		}
+
+	}
+
+	return out
+}
+
+func pt2Ranges(seeds []int, translators [][][]int) int {
+	lowest := 1 << 62
+
+	for i := 0; i < len(seeds); i += 2 {
+		r := []Range{{seeds[i], seeds[i] + seeds[i+1] - 1}}
+		for _, t := range translators {
+			r = transformRange(r, t)
+		}
+
+		fmt.Printf("%v\n", r)
+		for _, r := range r {
+			if r[0] < lowest {
+				lowest = r[0]
 			}
 		}
-
-		l++
 	}
+
+	return lowest
 }
 
 func main() {
 	timeStart := time.Now()
-	seeds, translators := parseInput()
+	seeds, translators := parseInput("input.txt")
 
 	fmt.Printf("--- Day 5: If You Give A Seed A Fertilizer ---\n")
 	fmt.Printf("Part 1: %d\n", pt1(seeds, translators))
-	fmt.Printf("Part 2: %d\n", pt2(seeds, translators))
+	fmt.Printf("Part 2: %d\n", pt2Ranges(seeds, translators))
 	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
 }

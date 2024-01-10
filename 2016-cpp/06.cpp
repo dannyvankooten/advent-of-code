c5e0c021561740a3b4a77cf0859424c602328d47
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2024-01-10 19:02:49 +0100

    2016 day 6 in cpp

---
+++ 2016-cpp/06.cpp
@@ -0,0 +1,46 @@
+#include <chrono>
+#include <iostream>
+#include <unordered_map>
+
+int main() {
+    auto tstart = std::chrono::high_resolution_clock::now();
+
+    std::unordered_map<char, int> maps[8];
+    for (int col = 0; col < 8; col++) {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            maps[col][ch] = 0;
+        }
+    }
+    std::string input;
+    while (std::getline(std::cin, input)) {
+        for (int col = 0; col < 8; col++) {
+            maps[col][input[col]]++;
+        }
+    }
+
+    std::string pt1(8, 'a');
+    std::string pt2(8, 'a');
+
+    for (int col = 0; col < 8; col++) {
+        for (char ch = 'b'; ch <= 'z'; ch++) {
+            if (maps[col][ch] > maps[col][pt1[col]]) {
+                pt1[col] = ch;
+            }
+
+            if (maps[col][ch] < maps[col][pt2[col]]) {
+                pt2[col] = ch;
+            }
+        }
+    }
+
+    std::cout << "--- Day 6: Signals and Noise ---\n";
+    std::cout << "Part 1: " << pt1 << "\n";
+    std::cout << "Part 2: " << pt2 << "\n";
+
+    auto tstop = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(tstop - tstart);
+    std::cout << "Time: " << duration.count() << " μs"
+              << "\n";
+    return EXIT_SUCCESS;
+}

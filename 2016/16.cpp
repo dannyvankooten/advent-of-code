d59b75064c867c084523be4e88f52d74902ea640
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2024-01-12 09:03:41 +0100

    advent of code 2016 day 16

---
+++ 2016/16.cpp
@@ -0,0 +1,64 @@
+#include <chrono>
+#include <iostream>
+
+using std::string;
+
+// mutate original according to puzzle rules
+// length of returned string is twice the input + 1
+string step(string a) {
+    size_t l = a.size();
+    a.resize(l * 2 + 1);
+    a[l] = '0';
+
+    for (long i = l - 1, j = l + 1; i >= 0; i--, j++) {
+        a[j] = a[i] == '1' ? '0' : '1';
+    }
+    return a;
+}
+
+// create odd-length checksum for input string
+string checksum(string a) {
+    for (size_t i = 0, j = 0; i < a.length() - 1; i += 2, j += 1) {
+        a[j] = a[i] == a[i + 1] ? '1' : '0';
+    }
+
+    a.resize(a.length() / 2);
+    if (a.length() % 2 == 0) {
+        return checksum(a);
+    }
+
+    return a;
+}
+
+string fill_disk_of_size(std::string input, size_t sz) {
+    string state = input;
+    input.reserve(sz * 2);
+
+    while (state.length() < sz) {
+        state = step(state);
+    }
+    state.resize(sz);
+    state = checksum(state);
+    return state;
+}
+
+int main() {
+    auto tstart = std::chrono::high_resolution_clock::now();
+
+    string input;
+    std::getline(std::cin, input);
+
+    string pt1 = fill_disk_of_size(input, 272);
+    string pt2 = fill_disk_of_size(input, 35651584);
+
+    std::cout << "--- Day 16: Dragon Checksum ---\n";
+    std::cout << "Part 1: " << pt1 << "\n";
+    std::cout << "Part 2: " << pt2 << "\n";
+
+    auto tstop = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(tstop - tstart);
+    std::cout << "Time: " << duration.count() << " μs"
+              << "\n";
+    return EXIT_SUCCESS;
+}

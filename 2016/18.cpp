331a70f2b23d4802b5e6423c731699b6c156f6bd
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2024-01-13 11:11:57 +0100

    2016 day 18 use bitset instead of char array

--- 2016/18.cpp
+++ 2016/18.cpp
@@ -1,62 +1,59 @@
+#include <bitset>
 #include <chrono>
 #include <iostream>
-#include <string.h>
 
 int count_traps(std::string input, int nrows) {
-    char prev_row[100];
-    char cur_row[100];
-    int trapcount = 0;
+  int trapcount = 0;
 
-    for (int c = 0; c < 100; c++) {
-        prev_row[c] = input[c];
+  // store row in bitset that's larger than row width
+  // so we don't have to do any bounds checking inside the loop
+  std::bitset<102> prev_row;
+  std::bitset<102> cur_row;
 
-        if (input[c] == '^') {
-            trapcount += 1;
-        }
+  prev_row[0] = false;
+  prev_row[101] = false;
+
+  for (int c = 0; c < 100; c++) {
+    prev_row[c + 1] = input[c] == '^' ? true : false;
+    if (input[c] == '^') {
+      trapcount += 1;
+    }
+  }
+
+  int tiles;
+  for (int r = 1; r < nrows; r++) {
+    for (int c = 1; c <= 100; c++) {
+      tiles = (prev_row[c - 1] << 2) + (prev_row[c] << 1) + prev_row[c + 1];
+      cur_row[c] = (tiles == 0b110 || tiles == 0b011 || tiles == 0b100 ||
+                    tiles == 0b001);
     }
 
-    char tiles[3];
+    trapcount += static_cast<int>(cur_row.count());
+    prev_row = cur_row;
+  }
 
-    for (int r = 1; r < nrows; r++) {
-        for (int c = 0; c < 100; c++) {
-            tiles[0] = c == 0 ? '.' : prev_row[c - 1];
-            tiles[1] = prev_row[c];
-            tiles[2] = c == 100 - 1 ? '.' : prev_row[c + 1];
-
-            if (memcmp(tiles, "^^.", 3) == 0 || memcmp(tiles, ".^^", 3) == 0 ||
-                memcmp(tiles, "^..", 3) == 0 || memcmp(tiles, "..^", 3) == 0) {
-                trapcount++;
-                cur_row[c] = '^';
-            } else {
-                cur_row[c] = '.';
-            }
-        }
-
-        std::swap(prev_row, cur_row);
-    }
-
-    return nrows * 100 - trapcount;
+  return (nrows * 100) - trapcount;
 }
 
 int main() {
-    auto tstart = std::chrono::high_resolution_clock::now();
-    int pt1 = 0;
-    int pt2 = 0;
+  auto tstart = std::chrono::high_resolution_clock::now();
+  int pt1 = 0;
+  int pt2 = 0;
 
-    std::string input;
-    std::getline(std::cin, input);
+  std::string input;
+  std::getline(std::cin, input);
 
-    pt1 = count_traps(input, 40);
-    pt2 = count_traps(input, 400000);
+  pt1 = count_traps(input, 40);
+  pt2 = count_traps(input, 400000);
 
-    std::cout << "--- Day 18: Like a Rogue ---\n";
-    std::cout << "Part 1: " << pt1 << "\n";
-    std::cout << "Part 2: " << pt2 << "\n";
+  std::cout << "--- Day 18: Like a Rogue ---\n";
+  std::cout << "Part 1: " << pt1 << "\n";
+  std::cout << "Part 2: " << pt2 << "\n";
 
-    auto tstop = std::chrono::high_resolution_clock::now();
-    auto duration =
-        std::chrono::duration_cast<std::chrono::microseconds>(tstop - tstart);
-    std::cout << "Time: " << duration.count() << " μs"
-              << "\n";
-    return EXIT_SUCCESS;
+  auto tstop = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(tstop - tstart);
+  std::cout << "Time: " << duration.count() << " μs"
+            << "\n";
+  return EXIT_SUCCESS;
 }

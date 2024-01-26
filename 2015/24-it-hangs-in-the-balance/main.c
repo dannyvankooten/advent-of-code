0039ff34cf025387d0781f7ded11a139018d3921
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2024-01-26 13:34:54 +0100

    2015 day 24 part 1 (part 2 by hand..)

--- 2015/24-it-hangs-in-the-balance/main.c
+++ 2015/24-it-hangs-in-the-balance/main.c
@@ -12,26 +12,7 @@ int parse(const char *s, int weights[]) {
   return n;
 }
 
-struct config {
-  int qe;
-  int weights[3];
-};
-
-struct group {
-  int weights[8];
-  int nweights;
-};
-
-void solve(int weights[], struct group groups[3], int nweights) {
-  for (int i = 0; i < nweights; i++) {
-    for (int j = 0; j < nweights - i; j++) {
-      int k = nweights - i - j;
-
-      printf("%d\t%d\t%d\n", i, j, k);
-    }
-  }
-}
-
+// qsort callback
 int cmp_ints(const void *p1, const void *p2) {
   int a = *(int *)p1;
   int b = *(int *)p2;
@@ -42,27 +23,27 @@ int main() {
   clock_t start_t = clock_time();
 
   char input[1024];
-  read_input_file(input, 1024, "input_test.txt");
+  read_input_file(input, 1024, "input.txt");
   int weights[32];
   int nweights = parse(input, weights);
   qsort(weights, nweights, sizeof(int), cmp_ints);
-  
+  int weights_sum = 0;
   for (int i = 0; i < nweights; i++) {
-    printf("%d\n", weights[i]);
+    weights_sum += weights[i];
   }
 
-  printf("parsed %d numbers\n", nweights);
-  struct group groups[3] = {
-      {.nweights = 0},
-      {.nweights = 0},
-      {.nweights = 0},
-  };
-  solve(weights, groups, nweights);
+  int group_sum = weights_sum / 3;
+  uint64_t qe = 1;
+  for (int s = 0, i = 0; i < nweights && s < group_sum; i++) {
+    if (s + weights[i] < group_sum) {
+      s += weights[i];
+      qe *= weights[i];
+    }
+  }
 
-  int a1 = 0;
   int a2 = 0;
   printf("--- Day 24: It Hangs in the Balance ---\n");
-  printf("Part 1: %d\n", a1);
+  printf("Part 1: %ld\n", qe);
   printf("Part 2: %d\n", a2);
   printf("Time: %.2fms\n", clock_time_since(start_t));
   return EXIT_SUCCESS;

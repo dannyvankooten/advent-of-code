ac88b129c1d8d22eb3a205f2d8a4dad40222c3ea
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2024-01-31 10:06:39 +0100

    optimize 2018 day 12 a little

--- 2018-c/12.c
+++ 2018-c/12.c
@@ -6,20 +6,56 @@
 
 #define PUZZLE_NAME "Day 12: Subterranean Sustainability"
 
+static const unsigned PADDING = 200; // should be larger than # of cycles
+static const unsigned INITIAL_PLANTS = 99;
+static const unsigned WIDTH = INITIAL_PLANTS + PADDING * 2;
+
 struct Pattern {
   char in[5];
   char out;
 };
+
+unsigned long count_plants(const char plants[WIDTH], unsigned long offset) {
+  unsigned long count = 0;
+
+  // for part 2 I noticed the grid would stabilise
+  // and then shifts right by 100 every 100 iterations
+  // so I used that property to compute the answer...
+  // not sure how it holds for other puzzle inputs though
+  for (unsigned i = 0; i < WIDTH; i++) {
+    if (plants[i] == '#') {
+      count += (unsigned long)((long)i - (long)PADDING) + offset;
+    }
+  }
+
+  return count;
+}
+
+void growth_cycles(char plants[WIDTH], const struct Pattern patterns[],
+                   const unsigned npatterns, const unsigned ncycles) {
+  char next[WIDTH];
+  for (unsigned g = 0; g < ncycles; g++) {
+    memset(next, '.', WIDTH * sizeof(char));
+
+    // go over every plant
+    for (unsigned p = 2; p < WIDTH - 2; p++) {
+      for (unsigned r = 0; r < npatterns; r++) {
+        if (memcmp(patterns[r].in, &plants[p - 2], 5) == 0) {
+          next[p] = patterns[r].out;
+          break;
+        }
+      }
+    }
+
+    memcpy(plants, next, WIDTH * sizeof(char));
+  }
+}
 
 int main(void) {
   clock_t t = clock_time();
 
   char line[1024];
   fgets(line, 1024, stdin);
-
-  const unsigned PADDING = 256;
-  const unsigned INITIAL_PLANTS = 99;
-  const unsigned WIDTH = INITIAL_PLANTS + PADDING *2;
 
   // 20 generations
   // so 32 padding should be enough
@@ -33,6 +69,12 @@ int main(void) {
   unsigned npatterns = 0;
   struct Pattern patterns[64];
   while (fgets(line, 1024, stdin)) {
+
+    // we only need to know about the patterns that result into a plant
+    if (line[9] != '#') {
+      continue;
+    }
+
     memcpy(patterns[npatterns].in, line, 5 * sizeof(char));
     patterns[npatterns].out = line[9];
     npatterns++;
@@ -40,41 +82,10 @@ int main(void) {
 
   unsigned long pt1 = 0;
   unsigned long pt2 = 0;
-  char next[WIDTH];
-  for (unsigned g = 0; g < 200; g++) {
-    memset(next, '.', WIDTH * sizeof(char));
-
-    // go over every plant
-    for (unsigned p = 2; p < WIDTH - 2; p++) {
-      for (unsigned r = 0; r < npatterns; r++) {
-        if (memcmp(patterns[r].in, &plants[p - 2], 5) == 0) {
-          next[p] = patterns[r].out;
-          break;
-        }
-      }
-    }
-
-    // TODO: Swap pointers here
-    memcpy(plants, next, WIDTH * sizeof(char));
-
-    if (g == 19) {
-      for (unsigned i = 0; i < WIDTH; i++) {
-        if (plants[i] == '#') {
-          pt1 += (unsigned long) ((long) i - (long)PADDING);
-        }
-      }
-    }
-  }
-
-  // for part 2 I noticed the grid would stabilise
-  // and then shifts right by 100 every 100 iterations
-  // so I used that property to compute the answer...
-  // not sure how it holds for other puzzle inputs though
-  for (unsigned i = PADDING; i < WIDTH; i++) {
-    if (plants[i] == '#') {
-      pt2 += (unsigned long) ((long) i - (long)PADDING) + 49999999800ul;
-    }
-  }
+  growth_cycles(plants, patterns, npatterns, 20);
+  pt1 = count_plants(plants, 0);
+  growth_cycles(plants, patterns, npatterns, 180);
+  pt2 = count_plants(plants, 49999999800ul);
 
   printf("--- %s ---\n", PUZZLE_NAME);
   printf("Part 1: %ld\n", pt1);

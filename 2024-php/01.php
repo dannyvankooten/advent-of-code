1915a8c9abe1bedb9aff6e4eb9bed44d3ebb8509
Danny van Kooten <hi@dvk.co>
2024-12-01 10:23:04 +0100

    speed up part 2 by starting loop at previous index

--- 2024-php/01.php
+++ 2024-php/01.php
@@ -1,6 +1,8 @@
 <?php
 
 $time_start = microtime(true);
+
+// parse input into two sorted arrays
 $input = trim(file_get_contents('01.txt'));
 $a = [];
 $b = [];
@@ -15,33 +17,24 @@ sort($a);
 sort($b);
 
 // add up distances between two corresponding locations
-$diff_sum = 0;
+$pt1 = 0;
 for ($i = 0; $i < count($a); $i++) {
-	$diff_sum += abs($b[$i] - $a[$i]);
+	$pt1 += abs($b[$i] - $a[$i]);
 }
 
 // find number of occurences that item in $a occurs in $b
 $pt2 = 0;
-foreach ($a as $item) {
+for ($i = 0, $j = 0; $i < count($a); $i++) {
 	$occurs = 0;
-	foreach ($b as $other) {
-		if ($item == $other) {
-			$occurs++;
-		}
-
-		// array is sorted, so we can stop comparing once $b is larger
-		if ($other > $item) {
-			break;
-		}
+	for (; $j < count($b) && $b[$j] <= $a[$i]; $j++) {
+		if ($a[$i] == $b[$j]) $occurs++;
 	}
 
-	$pt2  += $item * $occurs;
+	$pt2 += $a[$i] * $occurs;
 }
 
-
-
 echo "Day 1: Historian Hysteria", PHP_EOL;
-echo "Part 1: ", $diff_sum, PHP_EOL;
+echo "Part 1: ", $pt1, PHP_EOL;
 echo "Part 1: ", $pt2, PHP_EOL;
 echo "Took ", microtime(true) - $time_start, " seconds", PHP_EOL;
 echo PHP_EOL;

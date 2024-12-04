cbabfe519256898112c872546978247c46173035
Danny van Kooten <hi@dvk.co>
2024-12-04 09:16:28 +0100

    2024 day 4 part 2

--- 2024-php/04.php
+++ 2024-php/04.php
@@ -1,11 +1,21 @@
 <?php
 $time_start = microtime(true);
 $input = trim(file_get_contents('04.txt'));
+$grid = explode("\n", $input);
+$grid = array_map('str_split', $grid);
+$dirs = [
+	[0, 1], 	// down
+	[0, -1], 	// up
+	[1, 0], 	// right
+	[-1, 0], 	// left
+	[1, 1], 	// south-east
+	[-1, -1], 	// north-west
+	[1, -1], 	// north-east
+	[-1, 1] 	// south-west
+];
 
 function walk(array $grid, int $row, int $col, array $d, int $char_index): bool {
-	$word = "XMAS";
-
-	// take one step
+	static $word = "XMAS";
 	$row += $d[1];
 	$col += $d[0];
 
@@ -13,7 +23,6 @@ function walk(array $grid, int $row, int $col, array $d, int $char_index): bool 
 		return true;
 	}
 
-	// base condition
 	if (
 		$row < 0
 		|| $col < 0
@@ -27,24 +36,12 @@ function walk(array $grid, int $row, int $col, array $d, int $char_index): bool 
 	return walk($grid, $row, $col, $d, $char_index+1);
 }
 
-$dirs = [
-	[0, 1], 	// down
-	[0, -1], 	// up
-	[1, 0], 	// right
-	[-1, 0], 	// left
-	[1, 1], 	// south-east
-	[-1, -1], 	// north-west
-	[1, -1], 	// north-east
-	[-1, 1] 	// south-west
-];
-
-$grid = explode("\n", $input);
-$grid = array_map('str_split', $grid);
+$pt1 = 0;
 for ($row = 0; $row < count($grid); $row++) {
 	for ($col = 0; $col < count($grid[$row]); $col++) {
 		if ($grid[$row][$col] !== 'X') continue;
 
-		// current grid pos == 'X', search all directions for M
+		// current grid pos == 'X', search all directions for XMAS
 		foreach ($dirs as $d) {
 			if (walk($grid, $row, $col, $d, 1)) {
 				$pt1++;
@@ -54,6 +51,23 @@ for ($row = 0; $row < count($grid); $row++) {
 }
 
 $pt2 = 0;
+for ($row = 1; $row < count($grid) - 1; $row++) {
+	for ($col = 1; $col < count($grid[$row]) -1; $col++) {
+		if ($grid[$row][$col] !== 'A') continue;
+
+		$a = $grid[$row-1][$col-1] . $grid[$row][$col] . $grid[$row+1][$col+1];
+		$b = $grid[$row-1][$col+1] . $grid[$row][$col] . $grid[$row+1][$col-1];
+
+		if (
+			($a === "MAS" && $b === "MAS")
+			|| ($a === "SAM" && $b === "SAM")
+			|| ($a === "MAS" && $b === "SAM")
+			|| ($a === "SAM" && $b === "MAS")
+		) {
+			$pt2 += 1;
+		}
+	}
+}
 
 echo "--- Day 4: Ceres Search ---", PHP_EOL;
 echo "Part 1: ", $pt1, PHP_EOL;

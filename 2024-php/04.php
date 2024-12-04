5bde3c936ccf228593cacf25bc7746b8da41de1d
Danny van Kooten <hi@dvk.co>
2024-12-04 19:38:11 +0100

    optimise day 4 by using strings instead of char array, which has better cache locality because a char is a php object whereas string char is c char

--- 2024-php/04.php
+++ 2024-php/04.php
@@ -1,8 +1,12 @@
 <?php
 $time_start = microtime(true);
 $input = trim(file_get_contents('04.txt'));
-$grid = explode("\n", $input);
-$grid = array_map('str_split', $grid);
+$grid = array_map(function($row) {
+	return '.' . $row . '.';
+}, explode("\n", $input));
+$placeholder = str_repeat('.', strlen($grid[0]));
+$grid = array_merge([$placeholder], $grid, [$placeholder]);
+
 $dirs = [
 	[0, 1], 	// down
 	[0, -1], 	// up
@@ -25,11 +29,7 @@ function walk(array $grid, int $row, int $col, array $d, int $char_index): int
 	}
 
 	if (
-		$row < 0
-		|| $col < 0
-		|| $row >= count($grid)
-		|| $col >= count($grid[$row])
-		|| $grid[$row][$col] !== $word[$char_index]
+		$grid[$row][$col] !== $word[$char_index]
 	) {
 		return 0;
 	}
@@ -39,7 +39,7 @@ function walk(array $grid, int $row, int $col, array $d, int $char_index): int
 
 $pt1 = 0;
 for ($row = 0; $row < count($grid); $row++) {
-	for ($col = 0; $col < count($grid[$row]); $col++) {
+	for ($col = 0; $col < strlen($grid[$row]); $col++) {
 		if ($grid[$row][$col] !== 'X') continue;
 
 		// current grid pos == 'X', search all directions for XMAS
@@ -51,7 +51,7 @@ for ($row = 0; $row < count($grid); $row++) {
 
 $pt2 = 0;
 for ($row = 1; $row < count($grid) - 1; $row++) {
-	for ($col = 1; $col < count($grid[$row]) - 1; $col++) {
+	for ($col = 1; $col < strlen($grid[$row]) - 1; $col++) {
 		if ($grid[$row][$col] !== 'A') continue;
 
 		// current grid position === 'A', search diagonals for M S combo

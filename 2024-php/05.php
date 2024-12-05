c67ba208c529e79accc5e89a18e47a364ea2767d
Danny van Kooten <hi@dvk.co>
2024-12-05 10:08:58 +0100

    clean-up day 5

--- 2024-php/05.php
+++ 2024-php/05.php
@@ -10,52 +10,42 @@ $updates = array_map(function($line) {
 	return explode(",", trim($line));
 }, explode("\n", trim($input[1])));
 
-
+// create a hashmap to store our ordering rules so we can check order rules in constant time
 $ordering_rules_map = [];
 foreach ($ordering_rules as $rule) {
 	$ordering_rules_map[($rule[0] << 8) + $rule[1]] = true;
 }
 
+function is_sorted_correctly(array $update): bool {
+	global $ordering_rules_map;
+
+	for ($i = 1; $i < count($update); $i++) {
+		$a = $update[$i-1];
+		$b = $update[$i];
+		if (isset($ordering_rules_map[($b << 8) + $a])) return false;
+	}
+
+	return true;
+}
 
 // for part 1, add up all middle numbers for correct updates
 $pt1 = 0;
-$incorrect_updates = [];
+$pt2 = 0;
 foreach ($updates as $update) {
-	$correct = true;
-
-	// create hashmap with the position for each page number in the update
-	$seen = [];
-	foreach ($update as $pos => $page_no) {
-		$seen[$page_no] = $pos;
-	}
-
-	// go over all rules to see if update is correct
-	foreach ($ordering_rules as $rule) {
-		if (isset($seen[$rule[0]]) && isset($seen[$rule[1]]) && $seen[$rule[0]] >= $seen[$rule[1]]) {
-			$correct = false;
-			break;
-		}
-	}
-
-	if ($correct) {
+	if (is_sorted_correctly($update)) {
 		$mid = $update[count($update) / 2];
 		$pt1 += $mid;
 	} else {
-		$incorrect_updates[] = $update;
+		// for part 2, sort each incorrect update according to ordering rules
+		usort($update, function($a, $b) use ($ordering_rules, $ordering_rules_map) {
+			if (isset($ordering_rules_map[($a << 8) + $b])) return -1;
+			if (isset($ordering_rules_map[($b << 8) + $a])) return 1;
+			return 0;
+		});
+
+		$mid = $update[count($update) / 2];
+		$pt2 += $mid;
 	}
-}
-
-// for part 2, sort each incorrect update according to ordering rules
-$pt2 = 0;
-foreach ($incorrect_updates as $update) {
-	usort($update, function($a, $b) use ($ordering_rules, $ordering_rules_map) {
-		if (isset($ordering_rules_map[($a << 8) + $b])) return -1;
-		if (isset($ordering_rules_map[($b << 8) + $a])) return 1;
-		return 0;
-	});
-
-	$mid = $update[count($update) / 2];
-	$pt2 += $mid;
 }
 
 echo "--- Day 5: Print Queue ---", PHP_EOL;

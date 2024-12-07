6596f13816764406a4f4791e064f71a521f66a15
Danny van Kooten <hi@dvk.co>
2024-12-07 08:34:46 +0100

    day 7 part 2

--- 2024-php/07.php
+++ 2024-php/07.php
@@ -3,17 +3,22 @@
 $time_start = microtime(true);
 $input = trim(file_get_contents('07.txt'));
 
-function solve(int $expected_result, int $left, array $numbers) {
+function solve(int $expected_result, int $left, array $numbers, bool $concat = false) {
 	if (count($numbers) === 0) {
 		return $left === $expected_result ? $expected_result : 0;
 	}
 
 	$right = array_pop($numbers);
-	$result = solve($expected_result, $left * $right, $numbers);
+	$result = solve($expected_result, $left * $right, $numbers, $concat);
 	if ($result) return $result;
 
-	$result = solve($expected_result, $left + $right, $numbers);
+	$result = solve($expected_result, $left + $right, $numbers, $concat);
 	if ($result) return $result;
+
+	if ($concat) {
+		$result = solve($expected_result, intval("$left$right"), $numbers, $concat);
+		if ($result) return $result;
+	}
 
 	return 0;
 }
@@ -28,9 +33,12 @@ $input = array_map(function($line) {
 }, explode("\n", $input));
 
 
+$pt1 = 0;
+$pt2 = 0;
 foreach ($input as [$expected, $numbers]) {
 	$left = array_pop($numbers);
-	$pt1 += solve($expected, $left, $numbers);
+	$pt1 += solve($expected, $left, $numbers, false);
+	$pt2 += solve($expected, $left, $numbers, true);
 }
 
 echo "--- Day 7: Bridge Repair ---", PHP_EOL;
@@ -40,3 +48,7 @@ echo "Took ", (microtime(true) - $time_start) * 1000, " ms", PHP_EOL;
 echo PHP_EOL;
 
 assert($pt1 === 303766880536);
+assert($pt2 === 337041851384440);
+// 333903416016 too low
+// 337041851395637 too high
+// 337041851395637 too high

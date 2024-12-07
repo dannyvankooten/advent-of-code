058832ec80428a676fa5ae8ddba353f138fc433c
Danny van Kooten <hi@dvk.co>
2024-12-07 08:46:33 +0100

    optimise a bit

--- 2024-php/07.php
+++ 2024-php/07.php
@@ -4,23 +4,16 @@ $time_start = microtime(true);
 $input = trim(file_get_contents('07.txt'));
 
 function solve(int $expected_result, int $left, array $numbers, bool $concat = false) {
+	if ($left > $expected_result) {
+		return 0;
+	}
+
 	if (count($numbers) === 0) {
-		return $left === $expected_result ? $expected_result : 0;
+		return $left === $expected_result;
 	}
 
 	$right = array_pop($numbers);
-	$result = solve($expected_result, $left * $right, $numbers, $concat);
-	if ($result) return $result;
-
-	$result = solve($expected_result, $left + $right, $numbers, $concat);
-	if ($result) return $result;
-
-	if ($concat) {
-		$result = solve($expected_result, intval("$left$right"), $numbers, $concat);
-		if ($result) return $result;
-	}
-
-	return 0;
+	return solve($expected_result, $left * $right, $numbers, $concat) || solve($expected_result, $left + $right, $numbers, $concat) || ($concat && solve($expected_result, intval("$left$right"), $numbers, $concat));
 }
 
 $input = array_map(function($line) {
@@ -32,13 +25,12 @@ $input = array_map(function($line) {
 	return [intval($expected), array_reverse($numbers)];
 }, explode("\n", $input));
 
-
 $pt1 = 0;
 $pt2 = 0;
 foreach ($input as [$expected, $numbers]) {
 	$left = array_pop($numbers);
-	$pt1 += solve($expected, $left, $numbers, false);
-	$pt2 += solve($expected, $left, $numbers, true);
+	$pt1 += solve($expected, $left, $numbers, false) ? $expected : 0;
+	$pt2 += solve($expected, $left, $numbers, true) ? $expected : 0;
 }
 
 echo "--- Day 7: Bridge Repair ---", PHP_EOL;
@@ -49,6 +41,3 @@ echo PHP_EOL;
 
 assert($pt1 === 303766880536);
 assert($pt2 === 337041851384440);
-// 333903416016 too low
-// 337041851395637 too high
-// 337041851395637 too high

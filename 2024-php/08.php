156ac3ba7b5ed730ae3def850cd2cf880b23921a
Danny van Kooten <hi@dvk.co>
2024-12-08 19:37:31 +0100

    get rid of O(n) to find other antennas of same frequency

--- 2024-php/08.php
+++ 2024-php/08.php
@@ -8,31 +8,31 @@ class Antenna {
 	public function __construct(
 		public int $row,
 		public int $col,
-		public string $frequency,
 	) {}
 }
 
-function count_unique_antinodes(array $antennas, int $width, int $height, bool $pt2 = false)
+function count_unique_antinodes(array $frequency_antennas, int $width, int $height, bool $pt2 = false)
 {
 	$antinodes = [];
 
-	foreach ($antennas as $a) {
-		foreach ($antennas as $b) {
-			if ($a->frequency !== $b->frequency || ($a === $b)) continue;
+	foreach ($frequency_antennas as $frequency => $antennas) {
+		foreach ($antennas as $a) {
+			foreach ($antennas as $b) {
+				if ($a === $b) continue;
+				$rd = ($b->row - $a->row);
+				$cd = ($b->col - $a->col);
+				$r = $pt2 ? $a->row : $b->row;
+				$c = $pt2 ? $a->col : $b->col;
 
-			$rd = ($b->row - $a->row);
-			$cd = ($b->col - $a->col);
-			$r = $pt2 ? $a->row : $b->row;
-			$c = $pt2 ? $a->col : $b->col;
-
-			do {
-				$r += $rd;
-				$c += $cd;
-				if ($r < 0 || $c < 0 || $r >= $height || $c >= $width) {
-					break;
-				}
-				$antinodes["$r-$c"] = true;
-			} while ($pt2);
+				do {
+					$r += $rd;
+					$c += $cd;
+					if ($r < 0 || $c < 0 || $r >= $height || $c >= $width) {
+						break;
+					}
+					$antinodes["$r-$c"] = true;
+				} while ($pt2);
+			}
 		}
 	}
 
@@ -46,7 +46,10 @@ $antennas = [];
 for ($r = 0; $r < $height; $r++) {
 	for ($c = 0; $c < $width; $c++) {
 		if ($grid[$r][$c] === '.') continue;
-		$antennas[] = new Antenna($r, $c, $grid[$r][$c]);
+		if (!isset($antennas[$grid[$r][$c]])) {
+			$antennas[$grid[$r][$c]] = [];
+		}
+		$antennas[$grid[$r][$c]][] = new Antenna($r, $c);
 	}
 }
 $pt1 = count_unique_antinodes($antennas, $width, $height);

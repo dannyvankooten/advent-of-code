a0a5e6c69c255f1344e1e42c1f982a3b12c476d5
Danny van Kooten <hi@dvk.co>
2024-12-10 08:57:27 +0100

    add day number to template

--- 2024-php/new.sh
+++ 2024-php/new.sh
@@ -10,4 +10,5 @@ DAY=$1
 DAYNAME=$(printf "%02d\n" $DAY)
 
 cp template.php "$DAYNAME.php"
+sed -i "s/%%DAYNAME%%/$DAYNAME/" "$DAYNAME.php"
 wget --no-cookies --header="Cookie: session=$AOCSESSION" https://adventofcode.com/2024/day/$DAY/input -O $DAYNAME.txt
--- 2024-php/template.php
+++ 2024-php/template.php
@@ -1,12 +1,12 @@
 <?php
 
 $time_start = microtime(true);
-$input = trim(file_get_contents('DAY_HERE.txt'));
+$input = trim(file_get_contents('%%DAYNAME%%.txt'));
 
 $pt1 = 0;
 $pt2 = 0;
 
-echo "--- DAY_NAME_HERE ---", PHP_EOL;
+echo "--- Day %%DAYNAME%% ---", PHP_EOL;
 echo "Part 1: ", $pt1, PHP_EOL;
 echo "Part 2: ", $pt2, PHP_EOL;
 echo "Took ", (microtime(true) - $time_start) * 1000, " ms", PHP_EOL;

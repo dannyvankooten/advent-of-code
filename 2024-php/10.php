66fbde459a6d3be1b07a776618e662451e264bed
Danny van Kooten <hi@dvk.co>
2024-12-10 09:36:40 +0100

    day 10 part 2

--- 2024-php/10.php
+++ 2024-php/10.php
@@ -7,9 +7,17 @@ $map = array_map(function ($line) {
 }, explode("\n", $input));
 
 
-function hike(array $map, int $r, int $c) {
+function hike(array $map, int $r, int $c, array &$visited, bool $unique) {
 	if ($map[$r][$c] === 9) {
-		return ["$r-$c"];
+		if ($unique) {
+			if (!isset($visited["$r-$c"])) {
+				$visited["$r-$c"] = 1;
+				return 1;
+			}
+			return 0;
+		}
+
+		return 1;
 	}
 
 	$directions = [
@@ -18,7 +26,7 @@ function hike(array $map, int $r, int $c) {
 		[0, 1],
 		[0, -1],
 	];
-	$trailheads = [];
+	$score = 0;
 	foreach ($directions as $dir) {
 		$r2 = $r + $dir[0];
 		$c2 = $c + $dir[1];
@@ -30,23 +38,26 @@ function hike(array $map, int $r, int $c) {
 			continue;
 		}
 
-		$trailheads = array_merge($trailheads, hike($map, $r + $dir[0], $c + $dir[1]));
+		$score += hike($map, $r + $dir[0], $c + $dir[1], $visited, $unique);
 	}
 
-	return array_unique($trailheads);
+	return $score;
 }
 
+
 $pt1 = 0;
+$pt2 = 0;
 for ($r = 0; $r < count($map); $r++) {
 	for ($c = 0; $c < count($map[$r]); $c++) {
 		if ($map[$r][$c] === 0) {
-			$pt1 += count(hike($map, $r, $c));
+			$visited = [];
+			$pt1 += hike($map, $r, $c, $visited, true);
+
+			$visited = [];
+			$pt2 += hike($map, $r, $c, $visited, false);
 		}
 	}
 }
-
-
-$pt2 = 0;
 
 echo "--- Day 10 ---", PHP_EOL;
 echo "Part 1: ", $pt1, PHP_EOL;
@@ -54,3 +65,5 @@ echo "Part 2: ", $pt2, PHP_EOL;
 echo "Took ", (microtime(true) - $time_start) * 1000, " ms", PHP_EOL;
 echo PHP_EOL;
 
+assert($pt1 === 746);
+assert($pt2 === 1541);

7192fbae9e238c67d63c34860da1464984e67bbb
Danny van Kooten <hi@dvk.co>
2024-12-16 14:21:21 +0100

    verbosity

--- 2024-php/16.php
+++ 2024-php/16.php
@@ -23,10 +23,19 @@ $directions = [
 ];
 
 $queue = new \SplPriorityQueue();
-$queue->insert([$start, EAST, 0, []], 0);
+$queue->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
+$queue->insert([$start, EAST, []], 0);
 $visited = [];
 $pt1 = PHP_INT_MAX;
-foreach ($queue as [$pos, $d, $cost, $path]) {
+foreach ($queue as ['data' => $data, 'priority' => $priority]) {
+	[$pos, $d, $path] = $data;
+	$cost = -$priority;
+
+	// add position to path
+	$path[] = $pos;
+	$visited["{$pos[0]}-{$pos[1]}-$d"] = true;
+
+	// path to end node?
 	if ($pos === $end) {
 		if ($cost > $pt1) {
 			break;
@@ -36,30 +45,19 @@ foreach ($queue as [$pos, $d, $cost, $path]) {
 		foreach ($path as $p) {
 			$grid[$p[0]][$p[1]] = 'O';
 		}
-		$grid[$pos[0]][$pos[1]] = 'O';
 		continue;
 	}
 
-	// add position to path
-	$path[] = $pos;
-	$visited["{$pos[0]}-{$pos[1]}-$d"] = $cost;
-
-	// we can take a step
-	$left = $d === 0 ? 3 : $d - 1;
-	$right = $d === 3 ? 0 : $d + 1;
+	// add all possible steps to queue (but omit positions we've already been in)
+	$left = $d === EAST ? NORTH : $d - 1;
+	$right = $d === NORTH ? EAST : $d + 1;
 	foreach ([$left, $d, $right] as $dd) {
 		$dest = [$pos[0] + $directions[$dd][0], $pos[1] + $directions[$dd][1]];
-		if ($grid[$dest[0]][$dest[1]] !== '#') {
-			$new_cost = $cost + 1;
-			if ($dd != $d) {
-				$new_cost += 1000;
-			}
-
-			$prev_cost = $visited["{$dest[0]}-{$dest[1]}-$dd"] ?? PHP_INT_MAX;
-			if ($new_cost <= $prev_cost) {
-				$queue->insert([$dest, $dd, $new_cost, $path], -$new_cost);
-			}
+		if ($grid[$dest[0]][$dest[1]] === '#' || isset($visited["{$dest[0]}-{$dest[1]}-$dd"])) {
+			continue;
 		}
+
+		$queue->insert([$dest, $dd, $path], -($cost + ($dd == $d ? 1 : 1001)));
 	}
 }
 
@@ -71,3 +69,5 @@ echo "Part 2: ", $pt2, PHP_EOL;
 echo "Took ", (microtime(true) - $time_start) * 1000, " ms", PHP_EOL;
 echo PHP_EOL;
 
+assert($pt1 === 89460);
+assert($pt2 === 504);

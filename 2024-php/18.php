d927f137f16400618125081fe5f445e2cd4c6f62
Danny van Kooten <hi@dvk.co>
2024-12-18 09:42:13 +0100

    day 18 part 2

--- 2024-php/18.php
+++ 2024-php/18.php
@@ -12,49 +12,61 @@ for ($i = 0; $i < 1024; $i++) {
 	$grid[$y][$x] = '#';
 }
 
-dump($grid);
+function dijkstra(array $grid): int
+{
+	$start = [0, 0];
+	$end = [WIDTH - 1, HEIGHT - 1];
+	$queue = new Ds\Queue();
+	$queue->push([$start, 0]);
 
-$start = [0, 0];
-$end = [WIDTH - 1, HEIGHT - 1];
-$queue = new SplPriorityQueue();
-$queue->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
-$queue->insert($start, 0);
+	$directions = [
+		[0, -1],
+		[1, 0],
+		[0, 1],
+		[-1, 0],
+	];
+	$pt1 = PHP_INT_MAX;
+	$visited = [];
 
-$directions = [
-	[0, -1],
-	[1, 0],
-	[0, 1],
-	[-1, 0],
-];
+	foreach ($queue as [$pos, $steps]) {
+		if ($pos === $end) {
+			return $steps;
+		}
 
-$pt1 = PHP_INT_MAX;
-$visited = [];
-foreach ($queue as ['data' => $pos, 'priority' => $priority]) {
-	if ($pos === $end) {
-		$pt1 = -$priority;
-		break;
+		foreach ($directions as $d) {
+			$x2 = $pos[0] + $d[0];
+			$y2 = $pos[1] + $d[1];
+
+			// bounds check
+			if ($x2 < 0 || $y2 < 0 || $x2 >= WIDTH || $y2 >= HEIGHT) continue;
+
+			// obstacle check
+			if ($grid[$y2][$x2] === '#') continue;
+
+			// visited?
+			if ($visited["$x2-$y2"] ?? false) continue;
+
+			// mark location as visited and insert in queuephp q
+			$visited["{$x2}-{$y2}"] = true;
+			$queue->push([[$x2, $y2], $steps+1]);
+		}
 	}
 
-	foreach ($directions as $d) {
-		$x2 = $pos[0] + $d[0];
-		$y2 = $pos[1] + $d[1];
+	return PHP_INT_MAX;
+}
 
-		// bounds check
-		if ($x2 < 0 || $y2 < 0 || $x2 >= WIDTH || $y2 >= HEIGHT) continue;
+$pt1 = dijkstra($grid);
 
-		// obstacle check
-		if ($grid[$y2][$x2] === '#') continue;
+for ($i = 1024; $i < count($input); $i++) {
+	[$x, $y] = array_map('intval', explode(',', $input[$i]));
+	$grid[$y][$x] = '#';
+	$pt2 = $input[$i];
 
-		// visited?
-		if ($visited["$x2-$y2"] ?? false) continue;
-
-		// echo "Stepping to {$p[0]}, {$p[1]}\n";
-		$visited["{$x2}-{$y2}"] = true;
-		$queue->insert([$x2, $y2], $priority-1);
+	if (dijkstra($grid) === PHP_INT_MAX) {
+		break;
 	}
 }
 
-$pt2 = 0;
 
 echo "--- Day 18 ---", PHP_EOL;
 echo "Part 1: ", $pt1, PHP_EOL;

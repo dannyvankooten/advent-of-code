74201cb641ff4383cc8370fa15f0b9d9feb82a42
Danny van Kooten <hi@dvk.co>
2024-12-21 13:45:10 +0100

    optimise day 20 a bit

--- 2024-php/20.php
+++ 2024-php/20.php
@@ -37,72 +37,54 @@ function get_path(array $grid, int $x, int $y, int $ex, int $ey): array {
 		}
 	}
 
-	assert($x === $ex && $y === $ey);
-
+	$path[] = [$ex, $ey];
 	return $path;
 }
 
 function get_neighbors (array $grid, array $start, int $n): array {
 	global $w, $h;
-	$directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+	[$sx, $sy] = $start;
 	$neighbors = [];
-	$seen = [];
 
-	$queue = new \Ds\Queue();
-	$queue->push([$start[0], $start[1], 0]);
-	foreach ($queue as [$x, $y, $dist]) {
-		if ($grid[$y][$x] !== '#' && $dist > 0) {
-			$neighbors[] = [$x, $y, $dist];
-		}
-
-		if ($dist < $n) {
-			// take steps in all possible directions
-			foreach ($directions as [$dx, $dy]) {
-				$x2 = $x + $dx;
-				$y2 = $y + $dy;
-
-				// bounds check
-				if ($x2 < 0 || $y2 < 0 || $x2 >= $w || $y2 >= $h) continue;
-
-				if (!isset($seen["$x2-$y2"])) {
-					$seen["$x2-$y2"] = true;
-					$queue->push([$x2, $y2, $dist + 1]);
-				}
-			}
+	for ($y = max(0, $sy - $n); $y <= min($h-1, $sy + $n); $y++) {
+		for ($x = max(0, $sx - $n); $x <= min($w-1, $sx + $n); $x++) {
+			if ($grid[$y][$x] === '#' || ($dist = abs($sy - $y) + abs($sx - $x)) > $n) continue;
+			$neighbors["$x-$y"] = [$x, $y, $dist];
 		}
 	}
 
 	return $neighbors;
 }
 
-$pt1 = 0;
-$pt2 = 0;
 
 // first, create path from start to finish
 $path = get_path($grid, $start[0], $start[1], $end[0], $end[1]);
 $dist = count($path);
 $distance_to_end = [];
-foreach ($path as $dist => [$x, $y]) {
-	$distance_to_end["$x-$y"] = count($path) - $dist;
+foreach ($path as $d => [$x, $y]) {
+	$distance_to_end["$x-$y"] = $dist - $d;
 }
-$distance_to_end["{$end[0]}-{$end[1]}"] = 0;
 
-$savings = [];
+$cheat_scores = [
+	2 => 0,
+	20 => 0,
+];
+
 foreach ($path as $start_dist => $v) {
-	foreach (get_neighbors($grid, $v, 2) as [$xn, $yn, $cheat_dist]) {
-		$to_end = $cheat_dist + $distance_to_end["$xn-$yn"];
-		$diff = $dist - $start_dist - $to_end + 1;
-		if ($diff >= 100) $pt1++;
-	}
 
-	foreach (get_neighbors($grid, $v, 20) as [$xn, $yn, $cheat_dist]) {
-		$to_end = $cheat_dist + $distance_to_end["$xn-$yn"];
-		$diff = $dist - $start_dist - $to_end + 1;
-		if ($diff >= 100) $pt2++;
+	foreach (array_keys($cheat_scores) as $allowed_cheats) {
+		foreach (get_neighbors($grid, $v, $allowed_cheats) as [$xn, $yn, $cheat_dist]) {
+			$to_end = $cheat_dist + $distance_to_end["$xn-$yn"];
+			$diff = $dist - $start_dist - $to_end;
+			if ($diff >= 100) {
+				$cheat_scores[$allowed_cheats]++;
+			}
+		}
 	}
-
 }
 
+$pt1 = $cheat_scores[2];
+$pt2 = $cheat_scores[20];
 echo "--- Day 20 ---", PHP_EOL;
 echo "Part 1: ", $pt1, PHP_EOL;
 echo "Part 2: ", $pt2, PHP_EOL;
@@ -110,3 +92,4 @@ echo "Took ", (microtime(true) - $time_start) * 1000, " ms", PHP_EOL;
 echo PHP_EOL;
 
 assert($pt1 === 1293);
+assert($pt2 === 977747);

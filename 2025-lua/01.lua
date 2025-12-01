668a27237a5a7bab0154cdd519891f7205d250d4
Danny van Kooten <hi@dvk.co>
2025-12-01 11:39:22 +0100

    clean up day 1, add README

--- 2025-lua/01.lua
+++ 2025-lua/01.lua
@@ -1,36 +1,33 @@
-local lines = {}
-for line in io.lines() do
-  table.insert(lines, line)
-end
+local time_start = os.clock()
+local p = 50
+local pt1 = 0
+local pt2 = 0
 
-p = 50
-pt1 = 0
-pt2 = 0
-for i, l in ipairs(lines) do
-    dir = string.char(string.byte(l, 1))
-    amount = tonumber(string.sub(l, 2))
+for line in io.lines() do
+    local dir = string.sub(line, 0, 1)
+    local amount = string.sub(line, 2)
 
     if dir  == "L" then
-        modifier = -1
-    end
-    if dir == "R" then
-        modifier = 1
+        p = p - amount
+        while p < 0 do
+            p = 100 + p
+            pt2 = pt2 + 1
+        end
+    else
+        p = p + amount
+        while p > 99 do
+            p = 0 + (p - 100)
+            pt2 = pt2 + 1
+        end
     end
 
-    p = p + (modifier * amount)
-    while p < 0 do
-        p = 100 + p
-        pt2 = pt2 + 1
-    end
-    while p > 99 do
-        p = 0 + (p - 100)
-        pt2 = pt2 + 1
-    end
     if p == 0 then
         pt1 = pt1 + 1
     end
 
 end
 
+print("--- Day 1: Secret Entrance ---")
 print("Part 1", pt1)
 print("Part 2", pt2)
+print("Took ", os.clock() - time_start, "ms")

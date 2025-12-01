c89122742a9e2caa02dd3a2aebacad07bbb4f4e4
Danny van Kooten <hi@dvk.co>
2025-12-01 14:07:17 +0100

    rewrite day 1 to use modulo

--- 2025-lua/01.lua
+++ 2025-lua/01.lua
@@ -1,34 +1,28 @@
 local time_start = os.clock()
-local p = 50
 local pt1 = 0
 local pt2 = 0
+local p = 50
 
 for line in io.lines() do
     local dir = string.sub(line, 0, 1)
     local amount = string.sub(line, 2)
 
-    if dir  == "L" then
-        p = p - amount
-
-        while p < 0 do
-            p = 100 + p
-            pt2 = pt2 + 1
-        end
-    else
-        p = p + amount
-        while p > 99 do
-            p = 0 + (p - 100)
-            pt2 = pt2 + 1
-        end
+    if dir == "L" then
+        amount = -amount
     end
+
+    pt2 = pt2 + math.abs((p+amount) // 100)
+    p = (p + amount) % 100
 
     if p == 0 then
         pt1 = pt1 + 1
     end
-
 end
 
 print("--- Day 1: Secret Entrance ---")
 print("Part 1", pt1)
 print("Part 2", pt2)
 print("Took ", os.clock() - time_start, "ms")
+
+assert(pt1 == 999, "pt1 answer is incorrect")
+assert(pt2 == 6099, "pt2 answer is incorrect")

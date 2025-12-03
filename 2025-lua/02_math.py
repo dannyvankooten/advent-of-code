bcbf7c19e76a794a9a6f9a051101118392cb2680
Danny van Kooten <hi@dvk.co>
2025-12-03 13:39:45 +0100

    day 2 string solution in python

--- 2025-lua/02_math.py
+++ 2025-lua/02_math.py
@@ -5,29 +5,23 @@ import math
 time_start = time.time_ns()
 
 id_ranges = [list(map(int, v.split('-'))) for v in sys.stdin.read().split(',')]
-
 pt1 = 0
 for [start, end] in id_ranges:
 	for v in range(start, end+1):
-		full_id = str(v)
-		half_id = full_id[:int(len(full_id)/2)]
-		if half_id*2 == full_id:
+		digits = int(math.log10(v)) + 1
+
+		# uneven number of digits can never repeat twice
+		# skip to next log10 range
+		if digits & 1:
+			v *= 10
+
+		half_length = digits / 2
+		first_half = int(v // 10**half_length)
+		first_half_repeated = int(first_half * (10**half_length) + first_half)
+		if first_half_repeated == v:
 			pt1 += v
 
-
 pt2 = 0
-for [start, end] in id_ranges:
-	for v in range(start, end+1):
-		full_id = str(v)
-		for repeats in range(2, len(full_id)+1):
-
-			if len(full_id) % repeats != 0:
-				continue
-
-			partial_id = full_id[:int(len(full_id)/repeats)]
-			if partial_id*repeats == full_id:
-				pt2 += v
-				break
 
 print("--- Day 2: Gift Shop ---")
 print("Part 1", pt1)

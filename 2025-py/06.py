2a6ecd921cbd24c87ce9466bd40a52c832fec121
Danny van Kooten <hi@dvk.co>
2025-12-06 11:54:11 +0100

    2025 day 6 part 2

--- 2025-py/06.py
+++ 2025-py/06.py
@@ -5,8 +5,10 @@ import numpy as np
 from functools import reduce
 
 time_start = time.time_ns()
-lines = [l.strip().split() for l in sys.stdin.readlines()]
+input = sys.stdin.read()
+lines = [l.strip().split() for l in input.splitlines()]
 
+# part 1
 pt1 = 0
 lines = np.transpose(np.array(lines))
 for l in lines:
@@ -15,7 +17,50 @@ for l in lines:
 	else:
 		pt1 += reduce(lambda a, b: int(a)+int(b), l[:-1])
 
+# part 2
 pt2 = 0
+lines = [list(x) for x in input.splitlines()]
+
+# find columns
+width = len(lines[0])
+height = len(lines)
+result = 0
+op = ""
+
+for x in range(0, width):
+	d = ""
+
+	# walk down column, assembling digit
+	for y in range(0, height):
+		d += lines[y][x]
+
+	# if column of all spaces, move to next
+	if len(d.strip()) == 0:
+		pt2 += result
+		result = 0
+		# print()
+		continue
+
+	# set operator state
+	if d[-1] == '+' or d[-1] == '*':
+		op = d[-1]
+		d = d[:-1]
+
+		if op == "*":
+			result = 1
+
+	# now we have full digit and operation
+	if op == "*":
+		result *= int(d)
+	else:
+		result += int(d)
+
+# final column
+pt2 += result
+
+
+
+
 
 print("--- Day 06 --")
 print("Part 1", pt1)
@@ -23,4 +68,4 @@ print("Part 2", pt2)
 print("Took", (time.time_ns() - time_start) // 1_000_000, "ms")
 
 assert(pt1 == 4878670269096)
-# assert(pt2)
+assert(pt2 == 8674740488592)
